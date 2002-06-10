60456ffc810a2f63532e4351c2892108c002d785
dvoid <dvoid>
2002-06-10 16:11:05 +0000

    *** empty log message ***

---
+++ zerofps/src/Input.cpp
@@ -0,0 +1,107 @@
+#include "Input.h"
+
+
+Input::Input() {
+	cout<<"Sdl_Input initializing"<<endl;	
+	
+	//init sdl to read events
+	if(SDL_Init(SDL_INIT_EVENTTHREAD)<0){
+		cout<<"Sdl_Input dont want to work =("<<endl;
+		exit(1);
+	}
+	atexit(SDL_Quit);
+	
+	//reset all buttons to false
+	for(int i =0;i<400;i++) 
+		m_akButtonList[i]=false;
+	
+	
+	m_iGrabtime=SDL_GetTicks();
+};
+
+void Input::Update(void) {
+	while(SDL_PollEvent(&m_kEvent)) {
+		switch(m_kEvent.type) {
+			
+			//keyboard
+			case SDL_KEYDOWN:
+				m_akButtonList[m_kEvent.key.keysym.sym]=true;					    	
+    		break;			
+			case SDL_KEYUP:
+				m_akButtonList[m_kEvent.key.keysym.sym]=false;					    	
+    		break;    		
+    		
+    	//mouse    		
+    	case SDL_MOUSEBUTTONDOWN:
+    		switch(m_kEvent.button.button){
+    			case SDL_BUTTON_LEFT:
+    				m_akButtonList[MOUSELEFT]=true;
+    			break;
+    			case SDL_BUTTON_MIDDLE:
+    				m_akButtonList[MOUSEMIDDLE]=true;
+    			break;
+    			case SDL_BUTTON_RIGHT:
+    				m_akButtonList[MOUSERIGHT]=true;
+    			break;	
+    		}
+    		break;
+    	case SDL_MOUSEBUTTONUP:
+    		switch(m_kEvent.button.button){
+    			case SDL_BUTTON_LEFT:
+    				m_akButtonList[MOUSELEFT]=false;
+    			break;
+    			case SDL_BUTTON_MIDDLE:
+    				m_akButtonList[MOUSEMIDDLE]=false;
+    			break;
+    			case SDL_BUTTON_RIGHT:
+    				m_akButtonList[MOUSERIGHT]=false;
+    			break;	
+    		}    	
+    		break;
+		}	
+	}
+}
+
+void Input::MouseXY(int &iX,int &iY) {		
+	SDL_GetMouseState(&iX, &iY);
+}
+
+void Input::RelMouseXY(int &iX,int &iY) {		
+	SDL_GetRelativeMouseState(&iX, &iY);
+}
+
+void Input::ToggleGrab(void) {	
+	if(SDL_GetTicks()-m_iGrabtime>200) {
+		m_iGrabtime=SDL_GetTicks();
+		if(SDL_WM_GrabInput(SDL_GRAB_QUERY)==SDL_GRAB_OFF) {
+			GrabInput();
+	  } else {
+	  	ReleaseInput();
+	  }
+  } 	
+}
+
+void Input::ToggleGrab(bool bGrab) {
+	if(bGrab)
+		GrabInput();
+	else
+		ReleaseInput();
+}
+
+
+void Input::GrabInput(void) {
+	SDL_WM_GrabInput(SDL_GRAB_ON);
+ 	SDL_ShowCursor(SDL_DISABLE);
+}
+
+void Input::ReleaseInput(void) {
+	SDL_WM_GrabInput(SDL_GRAB_OFF);
+ 	SDL_ShowCursor(SDL_ENABLE);
+}
+	
+
+
+
+
+
+
---
+++ zerofps/src/Input.h
@@ -0,0 +1,75 @@
+#ifndef _INPUT_H_
+#define _INPUT_H_
+
+#include <SDL/SDL.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+enum Buttons {
+	ESCAPE=SDLK_ESCAPE,
+	LEFT=SDLK_LEFT,
+	RIGHT=SDLK_RIGHT,
+	UP=SDLK_UP,
+	DOWN=SDLK_DOWN,
+	SPACE=SDLK_SPACE,
+	F12=SDLK_F12,
+	F11=SDLK_F11,
+	F10=SDLK_F10,
+	F9=SDLK_F9,	
+	F8=SDLK_F8,	
+	F7=SDLK_F7,	
+	F6=SDLK_F6,
+	F5=SDLK_F5,	
+	F4=SDLK_F4,	
+	F3=SDLK_F3,	
+	F2=SDLK_F2,
+	F1=SDLK_F1,
+	KEY_A=SDLK_a,
+	KEY_W=SDLK_w,
+	KEY_S=SDLK_s,
+	KEY_D=SDLK_d,
+	PAGEUP=SDLK_PAGEUP,
+	PAGEDOWN=SDLK_PAGEDOWN,
+	HOME=SDLK_HOME,
+	END=SDLK_END,
+	
+	KEY_1=SDLK_1,
+	KEY_2=SDLK_2,
+	KEY_3=SDLK_3,
+	KEY_4=SDLK_4,
+	KEY_5=SDLK_5,
+	KEY_6=SDLK_6,
+	KEY_7=SDLK_7,
+	KEY_8=SDLK_8,
+	KEY_9=SDLK_9,
+	KEY_0=SDLK_0,
+	
+	MOUSELEFT=397,
+	MOUSEMIDDLE,
+	MOUSERIGHT
+
+};
+
+class Input {
+		bool m_akButtonList[400];
+		SDL_Event m_kEvent;
+		unsigned int m_iGrabtime;
+		
+		void GrabInput(void);
+		void ReleaseInput(void);
+	
+	
+	public:
+		Input();
+		
+		void Update(void);
+		inline bool Pressed(int iButton){return m_akButtonList[iButton];};
+		void MouseXY(int &iX,int &iY);
+		void RelMouseXY(int &iX,int &iY);
+		void ToggleGrab(void);
+		void ToggleGrab(bool bGrab);		
+//		inline unsigned int GetTicks(void) {return SDL_GetTicks();};
+};
+
+#endif
