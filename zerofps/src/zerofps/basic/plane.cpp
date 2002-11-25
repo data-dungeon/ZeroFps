d8f7d5802d08b34296fc4ccc1142e104b36e404b
vimus <vimus>
2002-11-25 16:20:02 +0000

    no message

---
+++ zerofps/src/zerofps/basic/plane.cpp
@@ -0,0 +1,18 @@
+#include "zfassert.h"
+#include "plane.h"
+
+
+bool Plane::PointInside(Vector3 kPoint)
+{
+	return ((kNormal.Dot(kPoint) + fD) <= 0);
+}
+
+bool Plane::SphereInside(Vector3 kCenter, float fRadius)
+{
+	float d = kNormal.Dot(kCenter) + fD;
+	if( d <= -fRadius )
+		return false;
+
+	return true;
+}
+
---
+++ zerofps/src/zerofps/basic/plane.h
@@ -0,0 +1,23 @@
+#ifndef _BASIC_PLANE_H_
+#define _BASIC_PLANE_H_
+
+#include "vector3.h"
+#include "basic_x.h"
+#include "os.h"
+
+class BASIC_API Plane 
+{
+public:
+	Vector3		kNormal;							// Normal of plane ( A,B,C in plane equation).
+	float		fD;									// Distance ( D in plane equation).
+
+	void		Set(float fX, float fY, float fZ, float fInD) {
+		kNormal.Set(fX,fY,fZ);
+		fD = fInD;
+		}
+	
+	bool PointInside(Vector3 kPoint);
+	bool SphereInside(Vector3 kCenter, float fRadius);
+};
+
+#endif
