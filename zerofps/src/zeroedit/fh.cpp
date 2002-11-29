93be3d0e59a586dcb7228b8d63e92b24586fbbe0
vimus <vimus>
2002-11-29 12:34:16 +0000

    no message

---
+++ zerofps/src/zeroedit/fh.cpp
@@ -0,0 +1,32 @@
+#include "fh.h"
+
+FHObject::FHObject() {
+	m_pkFps = static_cast<ZeroFps*>(g_ZFObjSys.GetObjectPtr("ZeroFps"));
+
+
+/*	AddProperty("MadProperty");
+	MadProperty* madp = dynamic_cast<MadProperty*>(GetProperty("MadProperty"));
+	madp->SetBasePtr(m_pkFps->GetMADPtr("../data/mad/tree.mad"));*/
+
+	m_iObjectType=OBJECT_TYPE_DYNAMIC;
+}
+
+FHObject::FHObject(const char* szMadName)
+{
+	m_pkFps = static_cast<ZeroFps*>(g_ZFObjSys.GetObjectPtr("ZeroFps"));
+
+	AddProperty("MadProperty");
+	MadProperty* madp = dynamic_cast<MadProperty*>(GetProperty("MadProperty"));
+	madp->SetBasePtr(m_pkFps->GetMADPtr(szMadName));
+	//szMadName
+	GetPos()=Vector3(0,0,0);
+	
+	m_iObjectType=OBJECT_TYPE_DYNAMIC;
+}
+
+
+void FHObject::HandleCollision(Object* pkOther,Vector3 kPos,bool bContinue)
+{
+
+}
+
---
+++ zerofps/src/zeroedit/fh.h
@@ -0,0 +1,19 @@
+#ifndef _ZEROEDIT_FH_H_
+#define _ZEROEDIT_FH_H_
+
+#include "../zerofps/engine/objects.pkg"
+#include "../zerofps/basic/basic.pkg"
+#include "../zerofps/engine/engine.pkg"
+
+class FHObject: public Object {
+	private:
+		ZeroFps *m_pkFps;
+
+	public:
+		FHObject();
+		FHObject(const char* szMadName);
+		void HandleCollision(Object* pkObject,Vector3 kPos,bool bContinue);
+};
+
+
+#endif
