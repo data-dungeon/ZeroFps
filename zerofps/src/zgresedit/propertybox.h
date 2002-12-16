c76fba0ecf7d53a833c2b030ab717288aee297f6
zeblar <zeblar>
2002-12-16 14:35:07 +0000

    no message

---
+++ zerofps/src/zgresedit/propertybox.h
@@ -0,0 +1,21 @@
+#ifndef _PROPERTYBOX_H
+#define _PROPERTYBOX_H
+
+#include "dlgbox.h"
+
+class PropertyBox : public DlgBox  
+{
+public:
+	void SetEditWnd(ZGuiWnd* pkWnd);
+	PropertyBox(GuiBuilder* pkGuiBuilder);
+	virtual ~PropertyBox();
+
+	bool Create(int x, int y, int w, int h, ZGuiWndProc pkWndProc);
+	bool OnOpen(int x=-1, int y=-1);
+	bool OnClose(bool bSave);
+
+private:
+	ZGuiWnd* m_pkWndEdit;
+};
+
+#endif // #ifndef _PROPERTYBOX_H
