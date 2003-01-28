9c53f0ff9bc396202e38ec3175028ca12bf781cc
zeblar <zeblar>
2003-01-28 18:08:22 +0000

    no message

---
+++ zerofps/src/zerofps/script/gamescript.cpp
@@ -0,0 +1,28 @@
+// gamescript.cpp: implementation of the GameScript class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "gamescript.h"
+
+// package files
+#include "../engine/engine.pkg"
+#include "package_files/zfscript_bind.h"
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+GameScript::GameScript() : ZFScript()
+{
+	OpenPackageFiles();
+}
+
+GameScript::~GameScript()
+{
+
+}
+
+void GameScript::OpenPackageFiles()
+{
+	tolua_zfscript_bind_open(GetLua());
+}
---
+++ zerofps/src/zerofps/script/gamescript.h
@@ -0,0 +1,25 @@
+// gamescript.h: interface for the GameScript class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_GAMESCRIPT_H__F77A05D0_A671_42F8_80B0_D13C97C3E2D5__INCLUDED_)
+#define AFX_GAMESCRIPT_H__F77A05D0_A671_42F8_80B0_D13C97C3E2D5__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "script_x.h"
+#include "zfscript.h"
+
+class SCRIPT_API GameScript : public ZFScript
+{
+public:
+	GameScript();
+	virtual ~GameScript();
+
+	void OpenPackageFiles();
+
+};
+
+#endif // !defined(AFX_GAMESCRIPT_H__F77A05D0_A671_42F8_80B0_D13C97C3E2D5__INCLUDED_)
