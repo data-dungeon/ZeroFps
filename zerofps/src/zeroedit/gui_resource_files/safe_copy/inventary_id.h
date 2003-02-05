7c6ec41b61178eef96c947952fb2016b447f8483
zeblar <zeblar>
2003-02-05 19:11:19 +0000

    no message

---
+++ zerofps/src/zeroedit/gui_resource_files/safe_copy/inventary_id.h
@@ -0,0 +1,42 @@
+#ifndef _INVENTARY_ID_H
+#define _INVENTARY_ID_H
+
+enum inventaryID
+{
+	InventoryWnd = 71,
+	InventCloseBN = 73,
+	InventSlotsSB = 74,
+	InvetSlotBN1 = 75,
+	InvetSlotBN2 = 76,
+	InvetSlotBN3 = 77,
+	InvetSlotBN4 = 78,
+	InvetSlotBN5 = 79,
+	InvetSlotBN6 = 80,
+	InvetSlotBN7 = 81,
+	InvetSlotBN8 = 82,
+	InvetSlotBN9 = 83,
+	InvetSlotBN10 = 84,
+	InvetSlotBN11 = 85,
+	InvetSlotBN12 = 86,
+	InvetSlotBN13 = 87,
+	InvetSlotBN14 = 88,
+	InvetSlotBN15 = 89,
+	InvetSlotBN16 = 90,
+	InvetSlotBN17 = 91,
+	InvetSlotBN18 = 92,
+	InvetSlotBN19 = 93,
+	InvetSlotBN20 = 94,
+	InvetSlotBN21 = 95,
+	InvetSlotBN22 = 96,
+	InvetSlotBN23 = 97,
+	InvetSlotBN24 = 98,
+	InvetSlotBN25 = 99,
+	InvetSlotBN26 = 100,
+	InvetSlotBN27 = 101,
+	InvetSlotBN28 = 102,
+	InvetSlotBN29 = 103,
+	InvetSlotBN30 = 104,
+
+};
+
+#endif // #ifndef _INVENTARY_ID_H
