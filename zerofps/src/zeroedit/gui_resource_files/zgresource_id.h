1db98563cedf94d41baa8c7a986db69cca89741a
zeblar <zeblar>
2003-02-07 11:37:34 +0000

    no message

---
+++ zerofps/src/zeroedit/gui_resource_files/zgresource_id.h
@@ -0,0 +1,42 @@
+#ifndef _ZGRESOURCE_RC_ID_H
+#define _ZGRESOURCE_RC_ID_H
+
+enum zgresource_rcID
+{
+	PropertyDlg = 67,
+	ObjectRotZ = 103,
+	ObjectRotY = 102,
+	ObjectRotX = 101,
+	Label100 = 100,
+	ObjectPropertiesCancel = 99,
+	ObjectPropOK = 98,
+	RemovePropertyBn = 96,
+	AddPropertiesBn = 95,
+	ObjectPosZEB = 92,
+	ObjectPosYEB = 91,
+	PropertyValueSetEB = 90,
+	ObjectPosXEB = 89,
+	ObjectNameEB = 88,
+	ObTypeDecorationRb = 87,
+	ObTypeStaticDynamicRb = 86,
+	ObTypePlayerRb = 85,
+	ObTypeStaticRb = 84,
+	ObTypeDynamicRb = 83,
+	Label82 = 82,
+	Label81 = 81,
+	Label80 = 80,
+	Label79 = 79,
+	Label78 = 78,
+	Label77 = 77,
+	ObjectPropCloseButton = 76,
+	Label75 = 75,
+	PropertyCB = 93,
+	PropertyValuesCB = 92,
+	ChangeValueOKBn = 97,
+	Textbox104 = 104,
+	Label105 = 105,
+
+	ObjectTypeRadioGroup = 10171,
+};
+
+#endif // #ifndef _ZGRESOURCE_RC_ID_H
