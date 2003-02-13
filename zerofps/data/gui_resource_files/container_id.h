e142af2c5b28686c2c522f6656462ca05a9bc588
zeblar <zeblar>
2003-02-13 12:18:09 +0000

    no message

---
+++ zerofps/data/gui_resource_files/container_id.h
@@ -0,0 +1,13 @@
+#ifndef _CONTAINER_ID_H
+#define _CONTAINER_ID_H
+
+enum containerID
+{
+	ContainerWnd = 71,
+	ContainerClose = 72,
+	ContainerPrevPage = 73,
+	ContainerNextPage = 74,
+
+};
+
+#endif // #ifndef _CONTAINER_ID_H
---
+++ zerofps/data/gui_resource_files/inventary_id.h
@@ -0,0 +1,13 @@
+#ifndef _INVENTARY_ID_H
+#define _INVENTARY_ID_H
+
+enum inventaryID
+{
+	InventoryWnd = 71,
+	InventNextPage = 187,
+	InventPrevPage = 188,
+	InventCloseBN = 73,
+
+};
+
+#endif // #ifndef _INVENTARY_ID_H
