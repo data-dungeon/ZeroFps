6b5ce72665b6de24541f4da3f85a681310b42516
dvoid <dvoid>
2003-03-16 22:37:09 +0000

    *** empty log message ***

---
+++ zerofps/src/zerorts/zerorts.cpp
@@ -0,0 +1,72 @@
+#include "zerorts.h"
+
+ZeroRTS g_kZeroRTS("ZeroRTS",1024,768,16);
+
+ZeroRTS::ZeroRTS(char* aName,int iWidth,int iHeight,int iDepth): Application(aName,iWidth,iHeight,iDepth) { }
+
+void ZeroRTS::OnInit() 
+{
+	cout<<"julle"<<endl;
+	
+	pkConsole->Printf(" ZeroRTS");
+	pkConsole->Printf("--------------------------------");
+	pkConsole->Printf(" Does anything work yet?");
+	
+	//run autoexec script
+	if(!pkIni->ExecuteCommands("zerorts_autoexec.ini"))
+		pkConsole->Printf("No game_autoexec.ini.ini found");
+
+
+	Init();
+}
+
+void ZeroRTS::Init()
+{
+	//damn "#¤(="%#( lighting fix
+	glEnable(GL_LIGHTING );
+	
+	//initiate our camera
+	m_pkCamera=new Camera(Vector3(0,10,0),Vector3(0,0,0),90,1.333,0.25,250);	
+	
+	//disable zones modells
+	pkLevelMan->SetVisibleZones(false);
+
+
+}
+
+
+void ZeroRTS::OnIdle(void) 
+{
+	cout<<"blle"<<endl;
+	pkFps->SetCamera(m_pkCamera);		
+	pkFps->GetCam()->ClearViewPort();	
+	pkFps->DevPrintf("common","Active Propertys: %d",pkObjectMan->GetActivePropertys());
+			
+
+}
+
+void ZeroRTS::OnHud(void) 
+{	
+
+}
+
+void ZeroRTS::OnServerStart(void)
+{	
+}
+
+void ZeroRTS::OnClientStart(void)
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
