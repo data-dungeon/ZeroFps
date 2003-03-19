6f4da45ff276570d67cec0392958d40c0609d028
zeblar <zeblar>
2003-03-19 17:52:53 +0000

    no message

---
+++ zerofps/src/zerorts/userpanel.cpp
@@ -0,0 +1,57 @@
+// userpanel.cpp: implementation of the UserPanel class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "userpanel.h"
+#include "zerorts.h"
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+UserPanel::UserPanel(ZeroRTS* pkZeroRts, ZGuiWndProc oMainWndProc) 
+	: DlgBox( pkZeroRts->pkGui, pkZeroRts->pkInput, oMainWndProc)
+{
+	m_pkZeroRts = pkZeroRts;
+}
+
+UserPanel::~UserPanel()
+{
+
+}
+
+bool UserPanel::Create(int x, int y, char* szResourceFile, char* szDlgName)
+{
+	m_pkDlgBox = new ZGuiWnd(Rect(0,m_pkZeroRts->m_iHeight-192,m_pkZeroRts->m_iWidth,
+		m_pkZeroRts->m_iHeight), NULL,false,15424);
+
+	ZGuiSkin* pkMainWndSkin = new ZGuiSkin(0,0,0, 0,0,0, 0, 0);
+
+	//pkMainWndSkin->m_iBkTexID = m_pkZeroRts->pkTexMan->Load("piss.bmp", 0); // först misslyckas, vet inte varför...
+	pkMainWndSkin->m_iBkTexID = m_pkZeroRts->pkTexMan->Load("../data/textures/player_panel_rts.bmp", 0);
+	pkMainWndSkin->m_iBkTexAlphaID = m_pkZeroRts->pkTexMan->Load("../data/textures/player_panel_a.bmp", 0);
+	m_pkDlgBox->SetSkin(pkMainWndSkin);
+
+	m_pkGui->AddMainWindow(15424+1, m_pkDlgBox, "UserPanelDlg", m_oMainWndProc, false);
+
+	return true;
+}
+
+bool UserPanel::DlgProc( ZGuiWnd* pkWnd,unsigned int uiMessage,
+						 int iNumberOfParams,void *pkParams )
+{
+	return true;
+}
+
+bool UserPanel::OnOpen(int x, int y)
+{
+	return true;
+}
+
+bool UserPanel::OnClose(bool bSave)
+{
+	return true;
+}
+
+
+
---
+++ zerofps/src/zerorts/userpanel.h
@@ -0,0 +1,37 @@
+// userpanel.h: interface for the UserPanel class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_USERPANEL_H__A8A3CF47_3B60_4C3E_9EB6_42353E9A8A2E__INCLUDED_)
+#define AFX_USERPANEL_H__A8A3CF47_3B60_4C3E_9EB6_42353E9A8A2E__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "../common/dialogbox.h"
+
+#define _DONT_MAIN
+
+class ZeroRTS;
+
+class UserPanel : public DlgBox
+{
+public:
+	UserPanel(ZeroRTS* pkZeroRts, ZGuiWndProc oMainWndProc);
+	virtual ~UserPanel();
+
+	bool Create(int x, int y, char* szResourceFile, char* szDlgName);
+	bool DlgProc( ZGuiWnd* pkWnd,unsigned int uiMessage,
+		int iNumberOfParams,void *pkParams );
+
+private:
+
+	bool OnOpen(int x, int y);
+	bool OnClose(bool bSave);
+
+	ZeroRTS* m_pkZeroRts;
+
+};
+
+#endif // !defined(AFX_USERPANEL_H__A8A3CF47_3B60_4C3E_9EB6_42353E9A8A2E__INCLUDED_)
