0ec2c4da10e42216b696741199add1bfb1ce2bea
zeblar <zeblar>
2003-03-20 13:23:09 +0000

    no message

---
+++ zerofps/src/zerofps/engine/pathbuilder.cpp
@@ -0,0 +1,97 @@
+// pathbuilder.cpp: implementation of the PathBuilder class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "pathbuilder.h"
+#include "heightmap.h"
+#include "pathfind.h"
+
+int* PathBuilder::m_piTerrain = NULL;
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+PathBuilder::PathBuilder(HeightMap* pkHeightMap, PathFind** m_ppkPathFind) 
+	: ZFObject("PathBuilder")
+{
+	m_pkHeightMap = pkHeightMap;
+	m_pkPathFind = *m_ppkPathFind;
+	m_piCostMap = NULL;
+}
+
+PathBuilder::~PathBuilder()
+{
+	if(m_piCostMap)
+		delete[] m_piCostMap;
+}
+
+void PathBuilder::Build(int pkObjectTypeCost[5])
+{
+	int iMapSize = m_pkHeightMap->GetSize();
+	int z,x;
+
+	if(m_piTerrain == NULL)
+	{
+		m_piTerrain = new int[iMapSize*iMapSize];
+
+		for(z=0; z<iMapSize; z++)
+			for(x=0; x<iMapSize; x++)
+			{
+				HM_vert* pkVert = m_pkHeightMap->GetVert(x,z);
+
+				int iIndex = z*iMapSize+x;
+				switch(pkVert->texture)
+				{
+				case 0: // slättland
+					m_piTerrain[iIndex] = GRASS;
+					break;
+				case 1: // vatten
+					m_piTerrain[iIndex] = WATER;
+					break;
+				case 2: // träsk
+					m_piTerrain[iIndex] = SWAMP;
+					break;
+				case 3: // väg
+					m_piTerrain[iIndex] = ROAD;
+					break;
+				case 4: // skog
+					m_piTerrain[iIndex] = WOOD;
+					break;
+				}
+			}
+	}
+
+	m_piCostMap = new int[iMapSize*iMapSize];
+
+	for(z=0; z<iMapSize; z++)
+		for(x=0; x<iMapSize; x++)
+		{
+			int iIndex = z*iMapSize+x;
+			switch(m_piTerrain[iIndex])
+			{
+			case GRASS:
+				m_piCostMap[iIndex] = pkObjectTypeCost[GRASS];
+				break;
+			case WOOD:
+				m_piCostMap[iIndex] = pkObjectTypeCost[WOOD];
+				break;
+			case SWAMP:
+				m_piCostMap[iIndex] = pkObjectTypeCost[SWAMP];
+				break;
+			case WATER:
+				m_piCostMap[iIndex] = pkObjectTypeCost[WATER];
+				break;
+			case ROAD:
+				m_piCostMap[iIndex] = pkObjectTypeCost[ROAD];
+				break;
+			}
+		}
+
+	m_pkPathFind = new PathFind(m_piCostMap, iMapSize, BLOCKED);
+}
+
+Point PathBuilder::GetMapTile(Vector3 pos)
+{
+	return Point((int)pos.x, (int)pos.y);
+}
---
+++ zerofps/src/zerofps/engine/pathbuilder.h
@@ -0,0 +1,33 @@
+// pathfindbuilder.h: interface for the PathFindBuilder class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_PATHFINDBUILDER_H__CA733ED4_A7A8_48B7_9A42_DEC57BBA575C__INCLUDED_)
+#define AFX_PATHFINDBUILDER_H__CA733ED4_A7A8_48B7_9A42_DEC57BBA575C__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "engine_x.h"
+#include "../basic/basic.pkg"
+
+class HeightMap;
+class PathFind;
+
+class ENGINE_API PathBuilder : public ZFObject
+{
+public:
+	Point GetMapTile(Vector3 pos);
+	void Build(int pkObjectTypeCost[5]);
+	PathBuilder(HeightMap* pkHeightMap, PathFind** m_ppkPathFind);
+	~PathBuilder();
+
+private:
+	HeightMap* m_pkHeightMap;
+	PathFind* m_pkPathFind;
+	int* m_piCostMap;
+	static int* m_piTerrain;
+};
+
+#endif // !defined(AFX_PATHFINDBUILDER_H__CA733ED4_A7A8_48B7_9A42_DEC57BBA575C__INCLUDED_)
