29ecc552df99a504378cc406fcf4d8051edf3006
dvoid <dvoid>
2003-04-01 14:22:15 +0000

    *** empty log message ***

---
+++ zerofps/src/common/p_serverinfo.cpp
@@ -0,0 +1,45 @@
+#include "p_serverinfo.h"
+
+P_ServerInfo::P_ServerInfo()
+{
+	strcpy(m_acName,"P_ServerInfo");
+	
+	m_iType=PROPERTY_TYPE_NORMAL;
+	m_iSide=PROPERTY_SIDE_CLIENT;
+	
+	bNetwork = true;
+
+
+	strcpy(m_kSInfo.m_acServerName,"Test Server");
+	
+	cout<<"server info created"<<endl;
+}
+
+void P_ServerInfo::PackTo(NetPacket* pkNetPacket)
+{
+	pkNetPacket->Write(&m_kSInfo,sizeof(m_kSInfo));
+}
+
+void P_ServerInfo::PackFrom(NetPacket* pkNetPacket)
+{
+	pkNetPacket->Read(&m_kSInfo, sizeof(m_kSInfo));
+	
+	cout<<"client got info"<<endl;
+
+}
+
+COMMON_API Property* Create_P_ServerInfo()
+{
+	return new P_ServerInfo();
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ zerofps/src/common/p_serverinfo.h
@@ -0,0 +1,44 @@
+/* Richard Svensson
+
+	Client & Server property that contains map information
+
+*/
+
+
+#ifndef _P_SERVERINFO_H_
+#define _P_SERVERINFO_H_
+
+#include "../zerofps/engine/objects.pkg"
+#include "../zerofps/engine/engine.pkg"
+#include "../zerofps/basic/basic.pkg"
+#include <iostream>
+#include "common_x.h"
+
+
+using namespace std;
+
+class COMMON_API P_ServerInfo: public Property {
+	private:
+	
+	public:
+		struct ServerInfo
+		{
+			char m_acServerName[64];
+			
+			
+		
+		} m_kSInfo; 
+
+	
+		P_ServerInfo();
+		void CloneOf(Property* pkProperty) { }
+
+		void PackTo(NetPacket* pkNetPacket);
+		void PackFrom(NetPacket* pkNetPacket);
+
+};
+
+COMMON_API Property* Create_P_ServerInfo();
+
+
+#endif
