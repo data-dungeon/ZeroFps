3f8fdd7367f321e81ff8e331fae451c572cf78b0
gubb <gubb>
2003-04-10 16:21:33 +0000

    no message

--- zerofps/src/zerofps/engine/property.cpp
+++ zerofps/src/zerofps/engine/property.cpp
@@ -445,6 +445,13 @@ string Property::ValueToString(void *pkValue, PropertyValues *pkPropertyValue)
 		kBuffer+=" ";
 		kBuffer+=pk_chBuffer;
 		return kBuffer;
+	
+	case VALUETYPE_CHARS:
+		{
+			//char* pcTemp;
+			//strcpy(kBuffer.c_str(), reinterpret_cast<char*>(pkValue));
+			return string(reinterpret_cast<char*>(pkValue));
+		}
 	};
 	string kStrTemp;
 	return kStrTemp;				
@@ -580,7 +587,17 @@ bool Property::StringToValue(string kValue, void *pkValue, PropertyValues *pkPro
 										
 										((Vector4*)pkValue)->Set(fTemp1,fTemp2,fTemp3,fTemp4); 
 										return true;
-										
+		
+			case VALUETYPE_CHARS:
+		if((pkPropertyValue->fUpperBound)!=FLT_MAX)
+			if(kValue.size()>(pkPropertyValue->fUpperBound))
+				return false;
+			if((pkPropertyValue->fLowerBound)!=FLT_MIN)
+				if(kValue.size()<(pkPropertyValue->fLowerBound))
+					return false;
+				strcpy(reinterpret_cast<char*>(pkValue),kValue.c_str());
+				return true;						
+								
 										
 				};
 				return false;	
