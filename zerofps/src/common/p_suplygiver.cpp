b7f29210e3d9ec4a1648318874012ef5421dfb80
dvoid <dvoid>
2003-04-22 14:20:19 +0000

    *** empty log message ***

--- zerofps/src/common/p_suplygiver.cpp
+++ zerofps/src/common/p_suplygiver.cpp
@@ -33,20 +33,20 @@ void P_SuplyGiver::Update()
 
 
 	if(m_pkServerUnit && m_pkServerInfo)
-	{
 		if(m_pkFps->GetGameTime() - m_fTime >2)
 		{
 			m_fTime = m_pkFps->GetGameTime();
-		
+	
 			int team = int(m_pkServerUnit->m_kInfo.m_Info2.m_cTeam);
-			
+		
+			cout<<"TEAM:"<<team<<endl;
+		
 			if(team >= 0 && team <= 4)			
 				m_pkServerInfo->m_kSInfo.m_aiPlayerResourses[team] += m_iAmount;
 			else			
 				cout<<"Gods dont need money"<<endl;
-	
+
 		}
-	}
 }
 
 
--- zerofps/src/zeroedit/zeroedit.cpp
+++ zerofps/src/zeroedit/zeroedit.cpp
@@ -1350,6 +1350,7 @@ Object* ZeroEdit::GetClosest(Vector3 kPos)
 
 void ZeroEdit::RegisterPropertys()
 {
+	pkPropertyFactory->Register("P_SuplyGiver", Create_P_SuplyGiver);
 	pkPropertyFactory->Register("P_UnitBuildAI", Create_P_UnitBuildAI);
 	pkPropertyFactory->Register("P_UnitAttackAI", Create_P_UnitAttackAI);
 	pkPropertyFactory->Register("P_UnitMoveAI", Create_P_UnitMoveAI);
