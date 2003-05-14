34b9317986d3f340acb77aea4decfcb330dffc91
dvoid <dvoid>
2003-05-14 18:36:12 +0000

    *** empty log message ***

---
+++ zerofpsv2/src/zerofpsv2/engine/res_texture.cpp
@@ -0,0 +1,34 @@
+#include "res_texture.h"
+
+ResTexture::ResTexture()
+{
+	m_iTextureID = NO_TEXTURE;
+
+}
+
+ResTexture::~ResTexture()
+{
+	if(m_iTextureID == NO_TEXTURE)
+		return;
+
+	TextureManager*	pkTex = static_cast<TextureManager*>(g_ZFObjSys.GetObjectPtr("TextureManager"));
+	pkTex->UnLoad(m_iTextureID);
+}
+
+
+bool ResTexture::Create(string strName)
+{
+	strTextureName = strName;
+
+	TextureManager*	pkTex = static_cast<TextureManager*>(g_ZFObjSys.GetObjectPtr("TextureManager"));
+	m_iTextureID = pkTex->Load(strName.c_str(), 0);
+		
+	return true;
+}
+
+ZFResource* Create__ResTexture()
+{
+	return new ResTexture;
+}
+
+
---
+++ zerofpsv2/src/zerofpsv2/engine/res_texture.h
@@ -0,0 +1,26 @@
+#ifndef _RES_TEXTURE_H_
+#define _RES_TEXTURE_H_
+
+#include "../basic/zfresource.h"
+#include "../render/texturemanager.h"
+#include "../basic/zfobjectmanger.h"
+
+class ResTexture : public ZFResource
+{
+private:
+
+public:
+	int		m_iTextureID;
+	string	strTextureName;
+
+	ResTexture();
+	~ResTexture();
+
+	bool Create(string strName);
+};
+
+
+#endif
+
+
+
