ce773a56c011e857149dea73675353cb7be2a318
zeblar <zeblar>
2003-06-23 13:00:26 +0000

    no message

---
+++ zerofpsv2/data/script/objects/test.lua
@@ -0,0 +1,13 @@
+function Create()
+	
+	InitObject();
+		InitProperty("P_Primitives3D");	
+			InitParameter("Radius","1");
+		InitProperty("PSystemProperty");
+			InitParameter("PSType","Smoke");
+		SetLocalPos(x,0,x);
+		AttachToClosestZone();
+	
+end
+
+
