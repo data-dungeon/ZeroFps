595f69f6ee874dca4087d4230e09fb9580eb30d1
dvoid <dvoid>
2003-07-03 13:00:21 +0000

    *** empty log message ***

--- zerofpsv2/src/rulesystem/rulesystem.cpp
+++
@@ -1,8 +0,0 @@
-#include "rulesystem.h"
-#include "character/characterstats.h"
-
-MCOMMON_API map<string, CharacterStats> g_kCharacters;
-MCOMMON_API vector<string> g_kSkills;
-MCOMMON_API map<string, SkillType> g_kSkillExps;
-MCOMMON_API vector<string> g_kAttributes;
-MCOMMON_API vector<string> g_kData;
