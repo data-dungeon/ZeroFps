ecb2d35136a99570ada72b28fdd00acb2e7f3702
dvoid <dvoid>
2003-07-04 16:11:18 +0000

    *** empty log message ***

---
+++ zerofpsv2/src/mcommon/p_clientcontrol.cpp
@@ -0,0 +1,19 @@
+#include "p_clientcontrol.h" 
+
+P_ClientControl::P_ClientControl()
+{
+	strcpy(m_acName,"P_ClientControl");		
+	m_iType=PROPERTY_TYPE_NORMAL;
+	m_iSide=PROPERTY_SIDE_SERVER|PROPERTY_SIDE_CLIENT;
+	
+	m_pkFps=static_cast<ZeroFps*>(g_ZFObjSys.GetObjectPtr("ZeroFps"));
+
+
+	cout<<"client Control created"<<endl;
+}
+
+
+Property* Create_P_ClientControl()
+{
+	return new P_ClientControl;
+}
---
+++ zerofpsv2/src/mcommon/p_clientcontrol.h
@@ -0,0 +1,27 @@
+#ifndef _P_CLIENTCONTROL_H_
+#define _P_CLIENTCONTROL_H_
+
+#include "../zerofpsv2/engine/property.h"
+#include "../zerofpsv2/engine/zerofps.h"
+#include "../zerofpsv2/script/zfscript.h"
+#include <iostream>
+#include <string.h>
+#include "mcommon_x.h"
+
+using namespace std;
+
+class MCOMMON_API P_ClientControl: public Property {
+	private:
+		ZeroFps* m_pkFps;
+
+
+	public:
+		
+		void CloneOf(Property* pkProperty) { }
+		P_ClientControl();
+		
+};
+
+MCOMMON_API Property* Create_P_ClientControl();
+
+#endif
