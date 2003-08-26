f68f1a51a746a4e9fada918d2c87b6362232ea3e
zeblar <zeblar>
2003-08-26 20:10:22 +0000

    no message

---
+++ zerofpsv2/src/mistclient/quickboard.h
@@ -0,0 +1,16 @@
+// quickboard.h: interface for the QuickBoard class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef _QUICKBOARD_H_
+#define _QUICKBOARD_H
+
+class QuickBoard  
+{
+public:
+	QuickBoard();
+	~QuickBoard();
+
+};
+
+#endif // #ifndef _QUICKBOARD_H_
