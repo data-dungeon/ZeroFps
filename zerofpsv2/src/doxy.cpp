cf5481a95eb249b0ea16c22dc94c1ac452165271
vimus <vimus>
2003-08-29 20:40:56 +0000

    *** empty log message ***

---
+++ zerofpsv2/src/doxy.cpp
@@ -0,0 +1,32 @@
+// File For DoxyDoc
+
+/**
+	\defgroup ZeroFPS ZeroFPS
+
+  The ZeroFPS Engine is made up off the following modules.
+*/
+
+/**
+	\defgroup Tools Tools
+
+	Tools used by ZeroFPS Engine and apps.
+*/
+
+/**
+	\defgroup MistLand MistLand
+
+	MistLand is a MORPG made with ZeroFPS.
+*/
+
+/**
+	\defgroup si ScriptGuide
+
+	Guide Over the scripts in ZFps/MistLand
+*/
+
+/** \mainpage My Personal Index Page
+ 
+  \section intro Introduction
+ 
+  This is the documentationen for the ZeroFps Engine, Tools and the game MistLand.
+*/
