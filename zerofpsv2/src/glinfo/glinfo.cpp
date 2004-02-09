086298682cc4783cdb0fab2c4859ab78f450d5d5
vimus <vimus>
2004-02-09 06:01:41 +0000

    *** empty log message ***

--- zerofpsv2/src/glinfo/glinfo.cpp
+++ zerofpsv2/src/glinfo/glinfo.cpp
@@ -1,34 +1,21 @@
-#include <GL/gl.h>
+//#include <windows.h>
 #include <iostream>
 #include <cstdio>
 #include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <vector>
 
 using namespace std;
 
-int main()
+void Log_DriverInfo()
 {
-	int m_iWidth = 640;
-	int m_iHeight = 480;
-	int m_iDepth = 16;
+	vector<string>	astrExt;
 
-	if(SDL_Init(SDL_OPENGL | SDL_INIT_NOPARACHUTE )<0)
-	{
-		cout<<"error initiating sdl"<<endl;
-		exit(1);
-	}	
-	
-	SDL_Surface* m_pkScreen= SDL_SetVideoMode(m_iWidth,m_iHeight,m_iDepth,SDL_OPENGL);
-
-
-	glViewport(0, 0,m_iWidth,m_iHeight);	
-	
-	
-
-	cout<<"Gl Version: "<<(char*)glGetString(GL_VERSION)<<endl;;
+	cout<<"Gl Version: "<<(char*)glGetString(GL_VERSION)<<endl;
 	cout<<"Gl Vendor:  "<<(char*)glGetString(GL_VENDOR)<<endl;
 	cout<<"Gl Render:  "<<(char*)glGetString(GL_RENDERER)<<endl;
-	cout<<"Gl Extensions:";
-	
+	cout<<"Gl Extensions:" << endl;
+
 	int i = 0;
 	char szExtName[256];
 	unsigned char* pcExt1 = const_cast<unsigned char*>(glGetString(GL_EXTENSIONS));
@@ -42,8 +29,32 @@ int main()
 	char* pcExt = (char*)pcExt1;
 	while(sscanf(pcExt, "%s", szExtName) != EOF) 
 	{
-		cout<<" Ext["<<i<<"]:"<<szExtName<<endl;
+		//cout<<" Ext["<<i<<"]:"<<szExtName<<endl;
+		astrExt.push_back( string(szExtName) );
 		i++;
 		pcExt += strlen(szExtName) + 1;
 	}
+
+	for(int i=0; i<astrExt.size(); i++)
+		cout<<" Ext["<<i<<"]:"<<astrExt[i]<<endl;
+}
+
+int main(int argc, char *argv[])
+{
+	int m_iWidth = 640;
+	int m_iHeight = 480;
+	int m_iDepth = 16;
+
+	if(SDL_Init(SDL_OPENGL | SDL_INIT_NOPARACHUTE )<0)
+	{
+		cout << "Error initiating SDL." << endl;
+		exit(1);
+	}	
+	
+	SDL_Surface* m_pkScreen= SDL_SetVideoMode(m_iWidth,m_iHeight,m_iDepth,SDL_OPENGL);
+	glViewport(0, 0,m_iWidth,m_iHeight);	
+
+	Log_DriverInfo();
+
+	return 0;
 }
