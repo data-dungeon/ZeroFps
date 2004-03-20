cfc940b776e49fb0d7225235e60376cd1a8524ec
dvoid <dvoid>
2004-03-20 09:07:57 +0000

    *** empty log message ***

--- zerofpsv2/src/zerofpsv2/basic/globals.h
+++ zerofpsv2/src/zerofpsv2/basic/globals.h
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+// some god makros
 #ifndef max
 #define max(a,b)            (((a) > (b)) ? (a) : (b))
 #endif
@@ -19,42 +20,46 @@ using namespace std;
 #define min(a,b)            (((a) < (b)) ? (a) : (b))
 #endif
 
-/*#define DEBUG_PRINT 0
-#define printf(x) { int apa = 2; }*/
-
-float const degtorad = float(57.2957795130823208767981548141052f);
-
-float BASIC_API DegToRad(float fAngle);				///< Convert a angle in degrees to radians.
-float BASIC_API RadToDeg(float fAngle);				///< Convert a angle in radians to degrees.
-
-void BASIC_API Gemens(char* aText);					///< Turn a string into lowcase.
-void BASIC_API IntToChar(char* aText,int iValue);
-
-int BASIC_API PowerOf2(int iVal);
-
-float BASIC_API Clamp(float fValue,float fBotom,float fTop);
-
-Vector3 BASIC_API GetYawVector2(float fAngleDeg);
-
-float BASIC_API GetYawAngle(Vector3 kDir);
-
-char* BoolStr(bool bFlag);
-void BASIC_API g_Logf(const char *fmt, ...);
-Vector3 BASIC_API RndColor();
-
-bool BASIC_API IsSameIgnoreCase(const char* szString1, const char* szString2);
-
+//some constants
 #define PId2	1.5707963279489
 #define PI		3.1415926535897932384626433832795f
 #define zf_pi	6.28318531f
 #define epsilon 1.0e-4f
-bool BASIC_API equal_f(float a, float b);
 
-float BASIC_API round2(float val);
+float const degtorad = float(57.2957795130823208767981548141052f);
 
+
+
+
+// angel funktions
+float BASIC_API DegToRad(float fAngle);				///< Convert a angle in degrees to radians.
+float BASIC_API RadToDeg(float fAngle);				///< Convert a angle in radians to degrees.
+Vector3 BASIC_API GetYawVector2(float fAngleDeg);
+float BASIC_API GetYawAngle(Vector3 kDir);
+
+
+//character handling
+void BASIC_API Gemens(char* aText);									///< Turn a string into lowcase.
+void BASIC_API IntToChar(char* aText,int iValue);				//
+char* BoolStr(bool bFlag);												//returns string "true" or "false" i think	
+bool BASIC_API IsSameIgnoreCase(const char* szString1, const char* szString2);
+
+//error handling
 extern char g_szFormatText[4096];	
-
 BASIC_API void ZFError(const char *fmt, ...);
 BASIC_API void ZFWarning(const char *fmt, ...);
+void BASIC_API g_Logf(const char *fmt, ...);
+
+
+// numerical funktions
+int BASIC_API PowerOf2(int iVal);										//returns closest integer whit the power of 2
+float BASIC_API Clamp(float fValue,float fBotom,float fTop);	//clamp value whitin botom and top
+bool BASIC_API equal_f(float a, float b);
+float BASIC_API round2(float val);										//round value to nearest integer
+
+
+//strange funktions =D
+Vector3 BASIC_API RndColor();												//randomize a colorvector...i think
+
 
 #endif
