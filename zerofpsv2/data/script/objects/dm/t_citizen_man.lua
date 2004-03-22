6004a0069a200887bacebac3b535e6e17921a654
zerom <zerom>
2004-03-22 15:00:10 +0000

    *** empty log message ***

---
+++ zerofpsv2/data/script/objects/dm/t_citizen_man.lua
@@ -0,0 +1,16 @@
+function Create()
+	
+	InitObject();
+		IsStatic();
+		InitProperty("P_Event");
+		InitProperty("P_Mad");							InitParameter("m_kMadFile","/data/mad/citizen_man.mad");
+			InitParameter("m_fScale","1");
+		SetParentObject();
+		SetReturnObject();
+end
+
+function Init()
+	PlayAnim(GetSelfID(), "walk");
+end
+
+
