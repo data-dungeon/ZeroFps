1d25b36a57bcedd11a9fa5bbdf7e5d52a29588a2
dvoid <dvoid>
2004-03-23 19:52:09 +0000

    *** empty log message ***

---
+++ zerofpsv2/src/mcommon/p_dmgun.cpp
@@ -0,0 +1,173 @@
+#include "p_dmgun.h" 
+#include "../zerofpsv2/engine_systems/propertys/p_mad.h"
+
+P_DMGun::P_DMGun()
+{
+	strcpy(m_acName,"P_DMGun");		
+	m_iType=PROPERTY_TYPE_NORMAL|PROPERTY_TYPE_RENDER;
+	m_iSide=PROPERTY_SIDE_SERVER|PROPERTY_SIDE_CLIENT;
+	
+	bNetwork = true;
+	
+	
+	m_fTimeBulletFired = 0;
+	m_fTimeFired = 0;
+	m_fBurstLength = 1;
+	m_bFireing =	false;
+	
+	m_kGunOffset.Set(0,1,0);
+	
+	//default gun
+	m_strName = 	"YberGun";
+	m_fFireRate = 	10;
+	m_fRange = 		10;
+	m_iAmmo = 		100;
+	m_iMaxAmmo = 	100;
+	m_kDir.Set(0,0,-1);
+	m_fRandom = 	1;
+}
+
+P_DMGun::~P_DMGun()
+{
+
+
+}
+
+void P_DMGun::Init()
+{
+	//cout<< "New GUN created"<<endl;
+
+}
+
+bool P_DMGun::Fire(Vector3 kTarget)
+{	
+	if(!m_bFireing)
+		m_fTimeBulletFired = m_pkObjMan->GetSimTime();
+	
+	
+	m_fTimeFired = m_pkObjMan->GetSimTime();
+	m_bFireing = true;
+	m_kDir = kTarget - (m_pkObject->GetWorldPosV() + m_kGunOffset);
+	
+	return true;
+}
+
+void P_DMGun::Update()
+{
+	float t = m_pkObjMan->GetSimTime();
+	
+	
+	if(m_pkObjMan->IsUpdate(PROPERTY_TYPE_RENDER))
+	{
+		for(int i = 0 ;i<m_kHitPos.size();i++)
+			m_pkZeroFps->m_pkRender->Sphere(m_kHitPos[i].first,0.2,1,Vector3(1,0,0),true);
+	
+		//vector<pair<Vector3,float> >::iterator kIte2;
+	   for ( vector<pair<Vector3,float> >::iterator kIte = m_kHitPos.begin(); kIte != m_kHitPos.end(); kIte++ )	
+		{	
+			if( (t - (*kIte).second ) > 0.4)
+			{
+				m_kHitPos.erase(kIte);
+				return;
+			}
+		}	
+	
+		return;
+	}
+	
+
+
+	if(!m_bFireing)	//firing?
+		return;
+	
+	
+	if( t - m_fTimeFired > m_fBurstLength)   //shuld we stop fireing
+	{	
+		m_bFireing = false;
+		return;
+	}
+
+
+	//how many bullets to fire?	
+	int iBulletsToFire = int((t - m_fTimeBulletFired) * m_fFireRate);
+	
+	//if no bullets are to be fired, we return
+	if(iBulletsToFire <= 0)
+		return;
+	
+	m_fTimeBulletFired = t;
+	
+	FireBullets(iBulletsToFire);
+}
+
+bool P_DMGun::FireBullets(int iAmount)
+{
+	float t = m_pkObjMan->GetSimTime();
+	Vector3 kStart = m_pkObject->GetWorldPosV() + m_kGunOffset;
+	
+	vector<Entity*> kObjects;		
+	m_pkObjMan->GetZoneObject()->GetAllObjects(&kObjects,false,true);	
+
+
+	for(int i =0;i<iAmount;i++)
+	{
+		Vector3 kDir = m_kDir +  Vector3( rand() % int(m_fRandom*1000) / 1000.0, rand() % int(m_fRandom*1000) / 1000.0,rand() % int(m_fRandom*1000) / 1000.0);
+	
+		float d;	
+		Vector3 cp;
+		float closest = 999999999;
+		Entity* pkClosest = NULL;
+		Vector3 kPickPos;
+		for(unsigned int i=0;i<kObjects.size();i++)
+		{
+			if(kObjects[i] == m_pkObject)
+				continue;
+		
+			//get mad property and do a linetest		
+			if(P_Mad* mp = (P_Mad*)kObjects[i]->GetProperty("P_Mad"))
+			{
+				if(mp->TestLine(kStart,kDir))
+				{	
+					cp = mp->GetLastColPos();
+					d = (kStart - cp).Length();
+	
+					if(d < closest)
+					{
+						closest = d;
+						pkClosest = kObjects[i];
+						kPickPos = cp;
+					}				
+				}
+			}				
+		}	
+		
+		if(pkClosest)
+		{
+			m_kHitPos.push_back(pair<Vector3,float>(kPickPos,t));			
+		}		
+	}
+	
+	return true;
+}
+
+
+vector<PropertyValues> P_DMGun::GetPropertyValues()
+{
+	vector<PropertyValues> kReturn(0);
+		
+
+	return kReturn;
+}
+
+
+Property* Create_P_DMGun()
+{
+	return new P_DMGun;
+}
+
+
+
+
+
+
+
---
+++ zerofpsv2/src/mcommon/p_dmgun.h
@@ -0,0 +1,63 @@
+#ifndef _P_DMGUN_H_
+#define _P_DMGUN_H_
+
+#include "../zerofpsv2/engine/property.h"
+#include "../zerofpsv2/engine/zerofps.h"
+#include "../zerofpsv2/script/zfscript.h"
+#include <iostream>
+#include <string.h>
+#include "mcommon_x.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MCOMMON_API P_DMGun: public Property {
+	private:
+		vector<PropertyValues> GetPropertyValues();
+	
+		
+		vector<pair<Vector3,float> >	m_kHitPos;
+		
+		float		m_fTimeFired;	
+		float		m_fTimeBulletFired;
+		float		m_fBurstLength;
+		bool		m_bFireing;
+		
+		Vector3	m_kGunOffset;
+		
+		//gun info
+		string	m_strName;
+		float		m_fFireRate;
+		float		m_fRange;		
+		int		m_iAmmo;
+		int		m_iMaxAmmo;		
+		Vector3	m_kDir;
+		float		m_fRandom;
+
+		
+		bool		FireBullets(int iAmount);
+
+		
+
+	public:
+		
+		P_DMGun();
+		~P_DMGun();
+
+		void CloneOf(Property* pkProperty) { }
+		
+		void Update();
+		void Init();
+		
+		bool Fire(Vector3 kTarget);
+		
+//		void Save(ZFIoInterface* pkPackage);
+//		void Load(ZFIoInterface* pkPackage);
+		
+};
+
+MCOMMON_API Property* Create_P_DMGun();
+
+#endif
+
