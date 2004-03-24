10a83feda36fe1634fb6d3d1e2875928360252cc
zeblar <zeblar>
2004-03-24 23:15:31 +0000

    no message

--- zerofpsv2/data/script/gui/dm_itemtransaction.lua
+++ zerofpsv2/data/script/gui/dm_itemtransaction.lua
@@ -16,9 +16,22 @@ function GUICreate()
 	Skin5 = { tex1="dm/sell_wnd.bmp", tex2="dm/bd_horz.bmp", tex3="dm/bd_vert.bmp", tex4="dm/bd_corner.bmp",
 	 tex1a="0", tex2a="dm/bd_horz_a.bmp", tex3a="dm/bd_vert_a.bmp", tex4a="dm/bd_corner_a.bmp", bkR=255, bkG=255, bkB=255, 
 	 borderR=255, borderG=255, borderB=255, bd_size=8, tile=0, trans=0 }
-	Skin6 = { tex1="dm/buy_wnd.bmp", tex2="dm/bd_horz.bmp", tex3="dm/bd_vert.bmp", tex4="dm/bd_corner.bmp",
+	Skin6 = { tex1="dm/sell_wnd.bmp", tex2="dm/bd_horz.bmp", tex3="dm/bd_vert.bmp", tex4="dm/bd_corner.bmp",
 	 tex1a="0", tex2a="dm/bd_horz_a.bmp", tex3a="dm/bd_vert_a.bmp", tex4a="dm/bd_corner_a.bmp", bkR=255, bkG=255, bkB=255, 
 	 borderR=255, borderG=255, borderB=255, bd_size=8, tile=0, trans=0 }
+	Skin7 = { tex1="dm/next_u.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255,
+	 bkG=255, bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+	Skin8 = { tex1="dm/next_d.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255, bkG=255,
+	 bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+	Skin9 = { tex1="dm/next_f.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255, bkG=255,
+	 bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+	Skin10 = { tex1="dm/prev_u.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255,
+	 bkG=255, bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+	Skin11 = { tex1="dm/prev_d.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255, bkG=255,
+	 bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+	Skin12 = { tex1="dm/prev_f.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255, bkG=255,
+	 bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+
 	-------------------------------------------------------------------------
 	-- Create all windows in correct order (parents before childrens etc.)
 	-- 0 = Wnd, 1 = Button, 2 = Checkbox, 3 = Combobox, 4 = Label, 5 = Listbox, 6 = Radiobutton,
@@ -27,18 +40,18 @@ function GUICreate()
 	CreateWnd(0,"ItemTransactionWnd","","",80,60,640,480,0)
 	ChangeSkin("ItemTransactionWnd","Skin4","Window")
 
-	CreateWnd(0,"ItemAddWnd","ItemTransactionWnd","",40,40,200,345,0)
+	CreateWnd(0,"ItemAddWnd","ItemTransactionWnd","",40,40,256,348,0)
 	ChangeSkin("ItemAddWnd","Skin6","Window")
 
-	CreateWnd(0,"ItemRemoveWnd","ItemTransactionWnd","",380,40,200,345,0)
+	CreateWnd(0,"ItemRemoveWnd","ItemTransactionWnd","",380,40,256,348,0)
 	ChangeSkin("ItemRemoveWnd","Skin5","Window")
 
-	CreateWnd(1,"RemoveItemBn","ItemRemoveWnd","Sell",60,318,70,20,0)
+	CreateWnd(1,"RemoveItemBn","ItemRemoveWnd","Sell",93,318,70,20,0)
 	ChangeSkin("RemoveItemBn","Skin1","Button up")
 	ChangeSkin("RemoveItemBn","Skin2","Button down")
 	ChangeSkin("RemoveItemBn","Skin3","Button focus")
 
-	CreateWnd(1,"AddItemBn","ItemAddWnd","Buy",60,317,64,20,0)
+	CreateWnd(1,"AddItemBn","ItemAddWnd","Buy",93,317,64,20,0)
 	ChangeSkin("AddItemBn","Skin1","Button up")
 	ChangeSkin("AddItemBn","Skin2","Button down")
 	ChangeSkin("AddItemBn","Skin3","Button focus")
@@ -47,6 +60,26 @@ function GUICreate()
 	ChangeSkin("ItemTransactionCloseBn","Skin1","Button up")
 	ChangeSkin("ItemTransactionCloseBn","Skin2","Button down")
 	ChangeSkin("ItemTransactionCloseBn","Skin3","Button focus")
+
+	CreateWnd(1,"ItemAddTransactionNextPageBn","ItemAddWnd","",230,318,20,20,0)
+	ChangeSkin("ItemAddTransactionNextPageBn","Skin7","Button up")
+	ChangeSkin("ItemAddTransactionNextPageBn","Skin8","Button down")
+	ChangeSkin("ItemAddTransactionNextPageBn","Skin9","Button focus")
+
+	CreateWnd(1,"ItemAddTransactionPrevPageBn","ItemAddWnd","",4,318,20,20,0)
+	ChangeSkin("ItemAddTransactionPrevPageBn","Skin10","Button up")
+	ChangeSkin("ItemAddTransactionPrevPageBn","Skin11","Button down")
+	ChangeSkin("ItemAddTransactionPrevPageBn","Skin12","Button focus")
+
+	CreateWnd(1,"ItemRemoveTransactionNextPageBn","ItemRemoveWnd","",230,318,20,20,0)
+	ChangeSkin("ItemRemoveTransactionNextPageBn","Skin7","Button up")
+	ChangeSkin("ItemRemoveTransactionNextPageBn","Skin8","Button down")
+	ChangeSkin("ItemRemoveTransactionNextPageBn","Skin9","Button focus")
+
+	CreateWnd(1,"ItemRemoveTransactionPrevPageBn","ItemRemoveWnd","",4,318,20,20,0)
+	ChangeSkin("ItemRemoveTransactionPrevPageBn","Skin10","Button up")
+	ChangeSkin("ItemRemoveTransactionPrevPageBn","Skin11","Button down")
+	ChangeSkin("ItemRemoveTransactionPrevPageBn","Skin12","Button focus")
 
 end
 
