2edbe2aed257735f08573ec18951c814e8380700
zeblar <zeblar>
2004-03-24 21:03:27 +0000

    no message

--- zerofpsv2/src/dark_metropolis/handleagents_dlg.cpp
+++ zerofpsv2/src/dark_metropolis/handleagents_dlg.cpp
@@ -26,4 +26,41 @@ void CHandleAgents::OnCommand(ZGuiWnd *pkMainWnd, string strClickName)
 		ShowWnd("MembersEquipBn", false); // dölj equip knappen
 		ShowWnd("MembersDropItemBn", false); // dölj drop knappen
 	}
+
+	int pos;
+	if((pos=strClickName.find("AgentsInHQBn")) != string::npos)
+	{
+		string temp(strClickName);
+		temp.erase(0,pos+strlen("AgentsInHQBn"));
+		
+		char* szButtons[] = {
+			"AgentsInHQBn1", "AgentsInHQBn2", "AgentsInHQBn3",
+			"AgentsInHQBn4", "AgentsInHQBn5", "AgentsInHQBn6",
+			"AgentsInHQBn7"
+		};
+
+		for(int i=0; i<7; i++)
+		{
+			if(strClickName != szButtons[i])
+				((ZGuiCheckbox*)GetWnd(szButtons[i]))->UncheckButton();
+		}
+	}
+
+	if((pos=strClickName.find("AgentsToHireBn")) != string::npos)
+	{
+		string temp(strClickName);
+		temp.erase(0,pos+strlen("AgentsInHQBn"));
+		
+		char* szButtons[] = {
+			"AgentsToHireBn1", "AgentsToHireBn2", "AgentsToHireBn3",
+			"AgentsToHireBn4", "AgentsToHireBn5", "AgentsToHireBn6",
+			"AgentsToHireBn7"
+		};
+
+		for(int i=0; i<7; i++)
+		{
+			if(strClickName != szButtons[i])
+				((ZGuiCheckbox*)GetWnd(szButtons[i]))->UncheckButton();
+		}
+	}
 }
