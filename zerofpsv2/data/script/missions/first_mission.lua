d4055490e5dad48375fc13f581343124bb67c183
zeblar <zeblar>
2004-04-02 08:27:04 +0000

    no message

---
+++ zerofpsv2/data/script/missions/first_mission.lua
@@ -0,0 +1,19 @@
+function OnMissionSuccess()
+--	Print("victory!, mission complete.");	
+end
+
+function IsMissionDone()
+
+	name = GetClosestDMCharacter();
+
+	Print(name)
+
+--	if obj_id == "linux" then
+--		Print("apan satt i grannen, skulle...\n");
+--	end
+
+end
+
+function IsMissionFailed()
+--	Print("you have failed!, no cash this time. return tro base!");
+end
