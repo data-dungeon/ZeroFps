5658a727b0ed95865b1a4ab8eb3894021083d2ad
zerom <zerom>
2004-04-07 22:11:48 +0000

    *** empty log message ***

--- zerofpsv2/data/script/objects/dm/t_citizen_man.lua
+++ zerofpsv2/data/script/objects/dm/t_citizen_man.lua
@@ -1,30 +1,59 @@
 function Create()
 	
 	InitObject();
-		InitProperty("P_Event");
-		InitProperty("P_Mad");													InitParameter("m_kMadFile","/data/mad/citizen_man.mad");
-			InitParameter("m_fScale","1");
+		InitProperty("P_Mad");	
+			InitParameter("m_kMadFile","/data/mad/citizen_man.mad");			
+			InitParameter("m_fScale","1");		
+
+		InitProperty("P_PfPath");
+		InitProperty("P_Track");
+		InitProperty("P_Sound");
+		InitProperty("P_DMCharacter");
 		InitProperty("P_ScriptInterface");
 		InitProperty("P_ShadowBlob");
-		InitProperty("P_DMCharacter");
-		SetParentObject();
-		SetReturnObject();
 
 end
 
 function FirstRun()
-	SISetHeartRate(SIGetSelfID(),2);
+	SISetHeartRate(SIGetSelfID(),4);
 end
 
 function Init()
-	SISetHeartRate(SIGetSelfID(),2);
+	SetMoveSpeed (SIGetSelfID(), 2.9);
 end
 
 function HeartBeat()
-	iClosest = GetDMCharacterClosest(SIGetSelfID());
 
+	if HavePath(SIGetSelfID()) then
+		return
+	end	
 
-	if iClosest > 0 then
-		KillCharacter (0);
+	if ( IsDead(SIGetSelfID()) == 1) then
+		AddToEntityVar (SIGetSelfID(), "deadtime", 1);
+
+		if GetEntityVar(SIGetSelfID(), "deadtime") > 9 then
+			Delete(SIGetSelfID());
+		end
+	
+		return
+	end
+
+	local pos = GetObjectPos(SIGetSelfID());
+	pos[1] = pos[1] + Random(20)-10;
+	pos[3] = pos[3] + Random(20)-10;
+
+	MakePathFind(SIGetSelfID(),pos);
+
+end
+
+function Dead()
+	PlayAnim(SIGetSelfID(), "die");
+	SetNextAnim(SIGetSelfID(), "dead");
+	ClearPathFind(SIGetSelfID());
+	PlaySound (SIGetSelfID(), "death/DEATH6.WAV");
+	SetEntityVar(SIGetSelfID, "deadtime", 0);
+
+	if Random(10) < 4 then
+		RunScript ("data/script/objects/dm/t_money.lua", SIGetSelfID());
 	end
 end
--- zerofpsv2/data/script/objects/dm/t_citizen_woman.lua
+++ zerofpsv2/data/script/objects/dm/t_citizen_woman.lua
@@ -1,11 +1,59 @@
 function Create()
 	
 	InitObject();
-		InitProperty("P_Mad");					InitParameter("m_kMadFile","/data/mad/citizen_woman.mad");
-			InitParameter("m_fScale","1");
-		SetParentObject();
-		SetReturnObject();
+		InitProperty("P_Mad");	
+			InitParameter("m_kMadFile","/data/mad/citizen_female.mad");			
+			InitParameter("m_fScale","1");		
+
+		InitProperty("P_PfPath");
+		InitProperty("P_Track");
+		InitProperty("P_Sound");
+		InitProperty("P_DMCharacter");
+		InitProperty("P_ScriptInterface");
+		InitProperty("P_ShadowBlob");
+
 end
 
+function FirstRun()
+	SISetHeartRate(SIGetSelfID(),4);
+end
 
+function Init()
+	SetMoveSpeed (SIGetSelfID(), 3);
+end
 
+function HeartBeat()
+
+	if HavePath(SIGetSelfID()) then
+		return
+	end	
+
+	if ( IsDead(SIGetSelfID()) == 1) then
+		AddToEntityVar (SIGetSelfID(), "deadtime", 1);
+
+		if GetEntityVar(SIGetSelfID(), "deadtime") > 9 then
+			Delete(SIGetSelfID());
+		end
+	
+		return
+	end
+
+	local pos = GetObjectPos(SIGetSelfID());
+	pos[1] = pos[1] + Random(20)-10;
+	pos[3] = pos[3] + Random(20)-10;
+
+	MakePathFind(SIGetSelfID(),pos);
+
+end
+
+function Dead()
+	PlayAnim(SIGetSelfID(), "die");
+	SetNextAnim(SIGetSelfID(), "dead");
+	ClearPathFind(SIGetSelfID());
+	PlaySound (SIGetSelfID(), "death/DEATH6.WAV");
+	SetEntityVar(SIGetSelfID, "deadtime", 0);
+
+	if Random(10) < 4 then
+		RunScript ("data/script/objects/dm/t_money.lua", SIGetSelfID());
+	end
+end
