64740aea4087099deb3d9f280db76d5d9c654d75
zerom <zerom>
2004-04-08 22:09:11 +0000

    elds nya

--- zerofpsv2/data/script/objects/dm/t_gun.lua
+++ zerofpsv2/data/script/objects/dm/t_gun.lua
@@ -2,13 +2,13 @@ function Create()
 	
 	InitObject();
 		InitProperty("P_Mad");	
-			InitParameter("m_kMadFile","/data/mad/gun.mad");			
+			InitParameter("m_kMadFile","/data/mad/dm/dm_handgun.mad");			
 			InitParameter("m_fScale","1");		
 		InitProperty("P_DMItem");	
 			InitParameter("name","gun");
 			InitParameter("sizex",1);
 			InitParameter("sizey",2);
-			InitParameter("icon","stol.bmp");
+			InitParameter("icon","handgun.bmp");
 			InitParameter("type","5");
 		InitProperty("P_DMGun");
 		InitProperty("P_ScriptInterface");
@@ -19,11 +19,11 @@ end
 function Init()
 	SetGunSound (SIGetSelfID(), "data/sound/9m_pistol.wav");
 	SetGunFireRate (SIGetSelfID(), 1);
-	SetGunDamage (SIGetSelfID(), 15);
+	SetGunDamage (SIGetSelfID(), 35);
 	SetGunRange (SIGetSelfID(), 30);
 	SetGunRandom (SIGetSelfID(), 0.1);
-	SetGunMaxAmmo (SIGetSelfID(), 7);
-	SetGunAmmo (SIGetSelfID(), 7);
+	SetGunMaxAmmo (SIGetSelfID(), 70);
+	SetGunAmmo (SIGetSelfID(), 70);
 end
 
 function FirstRun()
---
+++ zerofpsv2/data/script/objects/dm/t_handgun_clip.lua
@@ -0,0 +1,22 @@
+function Create()
+	
+	InitObject();
+		InitProperty("P_Mad");	
+			InitParameter("m_kMadFile","/data/mad/dm_handgun_clip.mad");			
+			InitParameter("m_fScale","1.0");		
+		InitProperty("P_DMItem");	
+			InitParameter("name","HandgunClip");
+			InitParameter("sizex",1);
+			InitParameter("sizey",1);
+			InitParameter("icon","handgun_clip.bmp");
+			InitParameter("type","4");
+		InitProperty("P_ScriptInterface");
+
+
+end
+
+function Init()
+end
+
+function FirstRun()
+end
