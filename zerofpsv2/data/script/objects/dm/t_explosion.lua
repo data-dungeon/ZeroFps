eaa4a45e7515b5bd0585f053b845b2defd6660f8
zerom <zerom>
2004-04-10 11:05:24 +0000

    *** empty log message ***

---
+++ zerofpsv2/data/script/objects/dm/t_explosion.lua
@@ -0,0 +1,19 @@
+function Create()
+	
+	InitObject();
+		InitProperty("P_Light");
+			InitParameter("Mode",0);
+			InitParameter("Quadratic_Atten",0.05);
+		InitProperty("P_PSystem");
+			InitParameter("PSType", "explosion");
+		InitProperty("P_ScriptInterface");
+		InitProperty("P_Sound");
+		SetParentObject();
+		SetReturnObject();
+end
+
+
+function Init()
+	PlaySound (SIGetSelfID(), "expl.wav");
+	Explosion (SIGetSelfID(), 3, 300);
+end
