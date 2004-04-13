79fb7f04f8681ef0aca379331bd33fa09a1ebac6
zeblar <zeblar>
2004-04-13 18:47:41 +0000

    no message

---
+++ zerofpsv2/data/script/missions/cops and c4.lua
@@ -0,0 +1,43 @@
+
+--///////////////////////////////////////////////////////////////////////////////
+--//
+--// Mission: Another mission
+--//
+--///////////////////////////////////////////////////////////////////////////////
+
+MissionInfo = { name="cops and c4", difficulty=1, xp=100, cash=500 } -- difficulty = Level
+
+MissionText = 
+{ 
+	short = "Another mission",
+	long = "Walk around in the town and collect XX number of XXX, when you have collected all XXX return to base."
+}
+
+function OnMissionStart()
+
+
+
+end
+
+function OnMissionSuccess()
+
+
+
+end
+
+function OnMissionFailed()
+
+
+
+end
+
+function IsMissionDone()
+
+
+end
+
+function IsMissionFailed()
+
+
+
+end
