9db9d72dfe73bc72455dda736731abdc3d7a6d74
zerom <zerom>
2004-04-20 20:01:00 +0000

    *** empty log message ***

---
+++ zerofpsv2/data/script/objects/dm/t_sign_dmpd.lua
@@ -0,0 +1,14 @@
+function Create()
+	
+	InitObject();
+		InitProperty("P_Mad");	
+			InitParameter("m_kMadFile","/data/mad/dm/dm_doorsign_dmpd.mad");						InitParameter("m_fScale","1");		
+
+end
+
+
+function Init()
+end
+
+function FirstRun()
+end
