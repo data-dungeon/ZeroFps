d25386613f4a33fbe966f58b8212eb1dafded734
zeblar <zeblar>
2004-04-20 13:20:13 +0000

    no message

--- zerofpsv2/src/mistserver/mistserver.cpp
+++ zerofpsv2/src/mistserver/mistserver.cpp
@@ -236,7 +236,7 @@ void MistServer::OnInit()
 	Init();
 
 	//init dm script interface (register script functions for gameplay)
-	DMLua::Init(m_pkObjectMan,m_pkScript);
+	DMLua::Init(m_pkObjectMan,m_pkScript,m_pkGuiMan);
 
 	//run autoexec script
 	if(!m_pkIni->ExecuteCommands("mistserver_autoexec.ini"))
