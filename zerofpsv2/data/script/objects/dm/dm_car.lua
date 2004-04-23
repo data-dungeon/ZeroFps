9a336635090fef8e1b1f8eafb0fac1f7ab1d8087
dvoid <dvoid>
2004-04-23 18:26:40 +0000

    *** empty log message ***

--- zerofpsv2/data/script/objects/dm/dm_car.lua
+++
@@ -1,9 +0,0 @@
-function Create()
-	
-	InitObject();
-		InitProperty("P_Mad");	
-			InitParameter("m_kMadFile","/data/mad/dm/dm_car.mad");
-			InitParameter("m_fScale","1");		
-		InitProperty("P_ShadowBlob");
-			InitParameter("scale","5.0");
-end
