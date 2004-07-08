2753b0d61b82f1e20d46c563481daac590aa78b0
dvoid <dvoid>
2004-07-08 11:08:07 +0000

    *** empty log message ***

---
+++ zerofpsv2/src/zerofpsv2/render/zfprogram.cpp
@@ -0,0 +1,142 @@
+#include "zfprogram.h"
+#include "render.h"
+ 
+ZFProgram::ZFProgram()
+{
+	m_iId = NO_FPROGRAM;
+	m_sFileName = "";
+	m_pkShader = NULL;
+}
+
+ZFProgram::~ZFProgram()
+{
+	if(m_iId == NO_FPROGRAM)
+		return;
+
+	UnLoad();
+}
+
+
+bool ZFProgram::Create(string  strName)
+{
+	return Load(strName);
+}
+
+int ZFProgram::CalculateSize()
+{
+	return 2;
+}
+
+bool ZFProgram::Load(string  strFile)
+{
+	if(!SetupShaderPointer())
+		return false;
+
+	//check if we have vertex program support
+	if(!m_pkShader->SupportFragmentProgram())
+		return false;
+
+	ZFVFile		m_kFile;
+	char*			cpProgram;
+	int 			iSize;
+	GLuint		iID;
+	
+	//open file
+	if(!m_kFile.Open(strFile,0,false))
+	{
+		cout<<"error opening file:"<<strFile<<endl;
+		return false;
+	}
+		
+	//get program size
+	iSize = m_kFile.GetSize();
+	
+	//allocated data buffer
+	cpProgram = new char[iSize];
+	
+	//load program into buffer
+	m_kFile.Read(cpProgram,iSize,1);
+	
+	//close file
+	m_kFile.Close();
+	
+	
+	//reset glERROR
+	glGetError();
+	
+	//Generate a program.
+	glGenProgramsARB(1, &iID);
+
+	//save the old active vertex program
+	int iOldID = m_pkShader->GetCurrentFragmentProgram();	
+	
+	//set current active program to the new created
+	m_pkShader->SetFragmentProgram(iID);
+
+	//upLoad the program.
+	glProgramStringARB(GL_FRAGMENT_PROGRAM_ARB, GL_PROGRAM_FORMAT_ASCII_ARB, iSize, cpProgram);		
+	
+	//error checking
+	int error = glGetError();
+	if(error != GL_NO_ERROR)
+	{
+		cout<<"Error occured while loading vertex program: "<<GetOpenGLErrorName(error)<<endl;
+		
+		int	pos;
+		char*	errormsg;
+		glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB,&pos);		
+		errormsg = (char*)glGetString(GL_PROGRAM_ERROR_STRING_ARB);
+		
+		cout<<"Error: "<<errormsg<<" On offset "<<pos<<endl;
+		
+		return false;
+	}
+		
+	//set id
+	m_iId = iID;
+	
+	//reactivate the old program
+	m_pkShader->SetFragmentProgram(iOldID);
+	
+
+	cout<<"Loaded fragment program: "<<strFile<<endl;
+	return true;
+}
+
+bool ZFProgram::UnLoad()
+{
+	if(m_iId == NO_FPROGRAM)
+		return false;
+
+	if(!SetupShaderPointer())
+		return false;
+
+	GLuint iID = m_iId;
+
+	//delete program
+	glDeleteProgramsARB(1,&iID);
+	
+	m_iId = NO_FPROGRAM;
+	
+	return true;
+}
+
+bool ZFProgram::SetupShaderPointer()
+{
+	if(m_pkShader)
+		return true;
+		
+ 	m_pkShader	= static_cast<ZShader*>(g_ZFObjSys.GetObjectPtr("ZShader")); 		
+
+	if(m_pkShader)
+		return true;
+	else
+		return false;
+}
+
+ZFResource* Create__ZFProgram()
+{
+	return new ZFProgram;
+}
+
+
---
+++ zerofpsv2/src/zerofpsv2/render/zfprogram.h
@@ -0,0 +1,51 @@
+#ifndef _ZFPROGRAM_H_
+#define _ZFPROGRAM_H_
+
+#include <iostream>
+#include "render_x.h"
+#include <vector>
+#include "../ogl/zfpsgl.h"
+#include "../basic/zfresource.h"
+#include "../basic/zfvfs.h"
+#include "zshader.h"
+
+using namespace std;
+
+
+#define	NO_FPROGRAM	-1
+
+/**	\brief	A OpenGL Fragment Program.
+		\ingroup Render
+
+This is a OpenGL fragment program that can be assigned to a material and used
+in the rendering.
+*/
+class RENDER_API ZFProgram : public ZFResource
+{
+	private:
+		ZShader*	m_pkShader;
+	
+		bool	SetupShaderPointer();
+		
+	public:
+		string	m_sFileName;
+		int		m_iId;
+
+
+		ZFProgram();
+		~ZFProgram();
+		
+		bool Create(string  strName);
+		int CalculateSize();
+
+
+		bool Load(string  strFile);
+		bool UnLoad();
+		
+};
+
+
+RENDER_API ZFResource* Create__ZFProgram();
+
+
+#endif
