5e5e08dbaf28e44854b2394b86f19646d3c489df
zerom <zerom>
2004-08-02 13:22:29 +0000

    *** empty log message ***

---
+++ zerofpsv2/data/sysdata/script/objects/r_board.lua
@@ -0,0 +1,30 @@
+function Create()
+	
+	InitObject();
+		InitProperty("P_Mad");
+			InitParameter("m_kMadFile","/data/mad/brada.mad");
+			--InitParameter("m_kMadFile","/data/mad/p_barrel.mad");
+			--InitParameter("m_kMadFile","/data/mad/physicsball.mad");
+			--InitParameter("m_kMadFile","/data/mad/dm/dm_barrel.mad");
+			InitParameter("m_fScale",1);
+			InitParameter("m_iShadowGroup","2");
+
+		InitProperty("P_Tcs");
+			InitParameter("polygontest","true");
+			InitParameter("radius",-1);
+			InitParameter("gravity","true");
+			InitParameter("group",1);
+			InitParameter("groupflag",0);
+			InitParameter("groupflag",1);			
+			InitParameter("airfriction",0.5);
+			InitParameter("mass",2);
+			InitParameter("inertia",0.4);
+			InitParameter("bounce",0.0);
+			InitParameter("friction",0.8);
+--			InitParameter("activemoment","false");
+			
+		SetParentObject();
+		SetReturnObject();
+		
+end
+
