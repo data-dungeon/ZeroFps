0bc65a5b8b14c11c91dcc13895058d5ed1b8f089
dvoid <dvoid>
2004-09-03 17:54:30 +0000

    *** empty log message ***

--- zerofpsv2/src/zeroed/zeroed.cpp
+++ zerofpsv2/src/zeroed/zeroed.cpp
@@ -193,7 +193,7 @@ void ZeroEd::CreateEditCameras()
 		m_pkCameraObject[i] = m_pkObjectMan->CreateObjectFromScript("data/script/objects/t_camedit.lua");
 		if(m_pkCameraObject[i]) 
 		{
-			m_pkCameraObject[i]->SetParent( m_pkObjectMan->GetWorldObject() );
+			m_pkCameraObject[i]->SetParent( m_pkObjectMan->GetWorldObject() );			
 			P_Camera* m_pkCamProp = (P_Camera*)m_pkCameraObject[i]->GetProperty("P_Camera");
 			m_pkCamProp->SetCamera(m_pkCamera[i]);
 			m_pkCameraObject[i]->GetSave() = false;
