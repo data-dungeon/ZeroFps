f6c1e6229b193a960225369ff58d689df5d85b37
zeblar <zeblar>
2004-09-11 08:19:49 +0000

    no message

--- zerofpsv2/data/mistlands/script/gui/ml_start.lua
+++ zerofpsv2/data/mistlands/script/gui/ml_start.lua
@@ -9,9 +9,14 @@ function GUICreate()
 	 bkG=255, bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
 	Skin2 = { tex1="misc_button_d.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255,
 	 bkG=255, bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
-	Skin3 = { tex1="wnd_bk.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255, bkG=0,
-	 bkB=0, borderR=255, borderG=255, borderB=255, bd_size=0, tile=1, trans=0 }
-	Skin4 = { tex1="misc_button_u.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=192,
+	Skin3 = { tex1="final/big_corner.bmp", tex2="0", tex3="0", tex4="0", tex1a="final/big_corner_a.bmp", tex2a="0",
+	 tex3a="0", tex4a="0", bkR=255, bkG=255, bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+	Skin4 = { tex1="wnd_bk.bmp", tex2="wndborder_left.bmp", tex3="wndborder_top.bmp", tex4="wndborder_corner.bmp",
+	 tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255, bkG=25, bkB=150, borderR=255, borderG=255, borderB=255, bd_size=0,
+	 tile=1, trans=0 }
+	Skin5 = { tex1="misc_button_u.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=255,
+	 bkG=25, bkB=25, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
+	Skin6 = { tex1="misc_button_u.bmp", tex2="0", tex3="0", tex4="0", tex1a="0", tex2a="0", tex3a="0", tex4a="0", bkR=192,
 	 bkG=192, bkB=255, borderR=255, borderG=255, borderB=255, bd_size=0, tile=0, trans=0 }
 	-------------------------------------------------------------------------
 	-- Create all windows in correct order (parents before childrens etc.)
@@ -19,15 +24,10 @@ function GUICreate()
 	-- 7 = Scrollbar, 8 = Slider, 9 = TabControl, 10 = Textbox, 11 = Treebox
 	-------------------------------------------------------------------------
 	CreateWnd(0,"MLStartWnd","","",0,0,800,600,0,0,3)
-	ChangeSkin("DMStartWnd","Skin3","Window")
-
-	CreateWnd(1,"LoadNewGameBn","MLStartWnd","Load",352,280,96,40,0,4,0)
-	ChangeSkin("LoadNewGameBn","Skin1","Button up")
-	ChangeSkin("LoadNewGameBn","Skin2","Button down")
-	ChangeSkin("LoadNewGameBn","Skin1","Button focus")
+	ChangeSkin("MLStartWnd","Skin4","Window")
 
 	CreateWnd(1,"QuitBn","MLStartWnd","Quit",352,426,96,40,0,4,0)
-	ChangeSkin("QuitBn","Skin1","Button up")
+	ChangeSkin("QuitBn","Skin5","Button up")
 	ChangeSkin("QuitBn","Skin2","Button down")
 	ChangeSkin("QuitBn","Skin1","Button focus")
 
@@ -37,11 +37,11 @@ function GUICreate()
 	ChangeSkin("SaveNewGameBn","Skin1","Button focus")
 
 	CreateWnd(1,"StarNewGameBn","MLStartWnd","New",352,230,96,40,0,4,0)
-	ChangeSkin("StarNewGameBn","Skin4","Button up")
+	ChangeSkin("StarNewGameBn","Skin6","Button up")
 	ChangeSkin("StarNewGameBn","Skin2","Button down")
 	ChangeSkin("StarNewGameBn","Skin1","Button focus")
 
-	CreateWnd(1,"ContinueGameBn","MLStartWnd","Continue",675,480,96,40,0,4,0)
+	CreateWnd(1,"ContinueGameBn","MLStartWnd","Continue",697,551,96,40,0,3,0)
 	ChangeSkin("ContinueGameBn","Skin1","Button up")
 	ChangeSkin("ContinueGameBn","Skin2","Button down")
 	ChangeSkin("ContinueGameBn","Skin1","Button focus")
@@ -50,6 +50,14 @@ function GUICreate()
 	ChangeSkin("CreditsBn","Skin1","Button up")
 	ChangeSkin("CreditsBn","Skin2","Button down")
 	ChangeSkin("CreditsBn","Skin1","Button focus")
+
+	CreateWnd(4,"Label41","MLStartWnd","",314,187,151,150,0,4,0)
+	ChangeSkin("Label41","Skin3","Label")
+
+	CreateWnd(1,"LoadNewGameBn","MLStartWnd","Load",352,280,96,40,0,4,0)
+	ChangeSkin("LoadNewGameBn","Skin1","Button up")
+	ChangeSkin("LoadNewGameBn","Skin2","Button down")
+	ChangeSkin("LoadNewGameBn","Skin1","Button focus")
 
 end
 
