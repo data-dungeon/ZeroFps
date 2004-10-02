31c79d9c9d66a6d15fe2ec1a1914a97734f116a2
dvoid <dvoid>
2004-10-02 19:48:12 +0000

    *** empty log message ***

--- zerofpsv2/src/mistserver/mistserver.cpp
+++ zerofpsv2/src/mistserver/mistserver.cpp
@@ -99,7 +99,7 @@ void MistServer::CreateEditCameras()
 void MistServer::OnInit() 
 {
 	m_pkZFVFileSystem->AddRootPath( string("../datafiles/mistlands") ,"/data");
-	m_pkZFVFileSystem->AddRootPath( string("../datafiles/dm") ,"/data");
+	//m_pkZFVFileSystem->AddRootPath( string("../datafiles/dm") ,"/data");
 	
 	m_pkConsole->Printf(" MistServer (mistland dedicated server)");
 	m_pkConsole->Printf("--------------------------------");
