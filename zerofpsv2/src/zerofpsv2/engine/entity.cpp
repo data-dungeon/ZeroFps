5a69d38ad981c18362b4c58fcb2ea706be16606c
dvoid <dvoid>
2004-11-22 21:49:17 +0000

    *** empty log message ***

--- zerofpsv2/src/zerofpsv2/engine/entity.cpp
+++ zerofpsv2/src/zerofpsv2/engine/entity.cpp
@@ -587,9 +587,6 @@ void Entity::PackTo(NetPacket* pkNetPacket, int iConnectionID)
 		SetNetUpdateFlag(iConnectionID,NETUPDATEFLAG_DELETEPROPLIST,false);
 	
 		pkNetPacket->Write((int) m_kNetDeletePropertyList.size() );
-
-		cout<<"hubba:"<<m_kNetDeletePropertyList.size()<<endl;
-		
 		for(unsigned int i=0; i<m_kNetDeletePropertyList.size(); i++)
 		{
 			pkNetPacket->Write_Str(	m_kNetDeletePropertyList[i]);
