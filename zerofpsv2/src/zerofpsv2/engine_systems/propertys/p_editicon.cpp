19c8bea319ecc146813967cc560d54ca71a5ac39
vimus <vimus>
2004-12-13 20:47:55 +0000

    *** empty log message ***

---
+++ zerofpsv2/src/zerofpsv2/engine_systems/propertys/p_editicon.cpp
@@ -0,0 +1,102 @@
+#include "p_editicon.h"
+#include "../../engine/zerofps.h"
+ 
+P_EditIcon::P_EditIcon()
+{
+	strcpy(m_acName,"P_EditIcon");		
+
+	m_pkTexMan=static_cast<TextureManager*>(g_ZFObjSys.GetObjectPtr("TextureManager"));	
+	m_pkRender=static_cast<Render*>(g_ZFObjSys.GetObjectPtr("Render"));		
+	m_pkFps=static_cast<ZeroFps*>(g_ZFObjSys.GetObjectPtr("ZeroFps"));			
+	
+	m_iType=PROPERTY_TYPE_RENDER;
+	m_iSide=PROPERTY_SIDE_CLIENT;
+	
+	m_kTexture="NONE";
+	m_fScale=1;	
+	m_kAxis.Set(1,1,1);
+	
+	UpdateSet();
+}
+
+void P_EditIcon::SetTexture(const char* acTexure)//,const char* acTex2)
+{
+	m_kTexture=acTexure;	
+	m_iTexture=m_pkTexMan->Load(acTexure,0);
+}
+
+void P_EditIcon::UpdateSet()
+{
+	SetTexture(m_kTexture.c_str());
+}
+
+void P_EditIcon::Update()
+{
+	if(!m_pkFps->GetCam()->GetFrustum()->SphereInFrustum(m_pkEntity->GetWorldPosV(),m_fScale))
+		return;
+		
+	Vector3 pos = m_pkEntity->GetIWorldPosV();
+
+	int iIcon = m_pkEntity->m_ucIcon;
+	switch(iIcon)
+	{
+		case 0:	return;	break;
+		case 1:	SetTexture("/data/textures/editicon/camera.bmp");	break;
+		case 2:	SetTexture("/data/textures/editicon/light.bmp");	break;
+	}
+
+	m_pkRender->DrawBillboard(m_pkFps->GetCam()->GetModelViewMatrix(),pos,m_fScale,m_iTexture, true);
+}
+
+vector<PropertyValues> P_EditIcon::GetPropertyValues()
+{
+	vector<PropertyValues> kReturn(2);
+ 
+	kReturn[0].kValueName = "texture";
+	kReturn[0].iValueType = VALUETYPE_STRING;
+	kReturn[0].pkValue    = (void*)&m_kTexture;
+
+	kReturn[1].kValueName = "scale";
+	kReturn[1].iValueType = VALUETYPE_FLOAT;
+	kReturn[1].pkValue    = (void*)&m_fScale;
+	return kReturn;
+}
+
+bool P_EditIcon::HandleSetValue( string kValueName ,string kValue )
+{
+	if(strcmp(kValueName.c_str(), "texture") == 0) 
+	{
+		SetTexture(kValue.c_str());
+		return true;
+	}
+
+	return false;
+}
+
+
+void P_EditIcon::Save(ZFIoInterface* pkPackage)
+{
+	char temp[128];
+	strcpy(temp,m_kTexture.c_str());	
+	pkPackage->Write((void*)&temp,128,1);
+
+	pkPackage->Write((void*)&m_fScale,4,1);
+}
+
+void P_EditIcon::Load(ZFIoInterface* pkPackage,int iVersion)
+{
+	char temp[128];
+	strcpy(temp,m_kTexture.c_str());	
+	pkPackage->Write((void*)&temp,128,1);
+
+	pkPackage->Write((void*)&m_fScale,4,1);
+}
+
+
+Property* Create_EditIcon()
+{
+	return new P_EditIcon;
+
+}
+
+
---
+++ zerofpsv2/src/zerofpsv2/engine_systems/propertys/p_editicon.h
@@ -0,0 +1,44 @@
+#ifndef _ENGINE_EDITICON_H_
+#define _ENGINE_EDITICON_H_
+
+#include "../../engine/property.h"
+#include "../../basic/zfsystem.h"
+#include <iostream>
+#include <string>
+#include "../engine_systems_x.h"
+
+using namespace std;
+
+
+class ENGINE_SYSTEMS_API P_EditIcon : public Property 
+{
+		TextureManager*	m_pkTexMan;
+		Render*				m_pkRender;	
+		ZeroFps*				m_pkFps;
+			
+		string				m_kTexture; 
+		
+		int					m_iTexture; 
+		Vector3				m_kAxis;	
+		float					m_fScale;
+		
+		vector<PropertyValues> GetPropertyValues();
+		bool HandleSetValue( string kValueName ,string kValue );		
+	
+	public:
+		P_EditIcon();
+		void Update();
+
+		void SetTexture(const char* acTexure);
+		void SetScale(float fScale) {m_fScale=fScale;};
+		void UpdateSet();
+		
+		void Save(ZFIoInterface* pkPackage);
+		void Load(ZFIoInterface* pkPackage,int iVersion);
+		
+};
+
+Property* Create_EditIcon();
+
+#endif
+
