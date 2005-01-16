9a84becf15051705381b65fb85b0180c694691c0
zeblar <zeblar>
2005-01-16 15:18:24 +0000

    no message

--- zerofpsv2/src/mistclient2/gui_equipwnd.cpp
+++ zerofpsv2/src/mistclient2/gui_equipwnd.cpp
@@ -63,7 +63,22 @@ void GuiMsgEquipmentDlg( string strMainWnd, string strController,
 	}
 }
 
-EquipmentDlg::EquipmentDlg() 
+EquipmentDlg::EquipmentDlg() :
+		m_rcHelm(		  97,  11,  97 + 64,  11 +  64 ),
+		m_rcGloves(		   7,  47,   7 + 64,  47 +  64 ),
+		m_rcAmulett(	 113,  84, 113 + 32,  84 +  32 ),
+		m_rcCloak(		 185,  50, 185 + 64,  50 +  64 ),
+		m_rcWeaponHand(   7, 124,   7 + 64, 124 + 128 ),
+		m_rcBody(		  80, 124,  80 + 96, 124 + 128 ),
+		m_rcShieldHand( 185, 125, 185 + 64, 125 + 128 ),
+		m_rcBracer1(	   7, 265,   7 + 32, 265 +  64 ),
+		m_rcBracer2(	  40, 265,  40 + 32, 265 +  64 ),
+		m_rcBelt(		  94, 265,  94 + 64, 265 +  32 ),
+		m_rcRing1(		 184, 267, 184 + 32, 267 +  32 ),
+		m_rcRing2(		 218, 267, 218 + 32, 267 +  32 ),
+		m_rcRing3(		 184, 301, 184 + 32, 301 +  32 ),
+		m_rcRing4(		 217, 301, 217 + 32, 301 +  32 ),
+		m_rcBoots(		  94, 309,  94 + 64, 309 +  64 )
 {
 	m_pkEquipmentWnd = NULL;
 	m_pkTexMan = g_kMistClient.m_pkTexMan;
@@ -124,4 +139,107 @@ void EquipmentDlg::OnMouseMove(bool bLeftButtonPressed, int mx, int my)
 {
 	
 	
+}
+
+void EquipmentDlg::OnDrop(int mx, int my)
+{
+
+}
+
+Rect EquipmentDlg::GetRect(EquipmentSlot eSlot, bool bScreenPos)
+{
+	if(m_pkEquipmentWnd == NULL)
+		return Rect(0,0,0,0);
+
+	Rect rc(0,0,0,0);
+
+	if(bScreenPos)
+		rc += m_pkEquipmentWnd->GetScreenRect();
+
+	switch(eSlot)
+	{
+	case EqS_Helm:			rc += m_rcHelm;
+	case EqS_Gloves:		rc += m_rcGloves;
+	case EqS_Amulett:		rc += m_rcAmulett;
+	case EqS_Cloak:		rc += m_rcCloak;
+	case EqS_WeaponHand:	rc += m_rcWeaponHand;
+	case EqS_Body:			rc += m_rcBody;
+	case EqS_ShieldHand:	rc += m_rcShieldHand;
+	case EqS_Bracer1:		rc += m_rcBracer1;
+	case EqS_Bracer2:		rc += m_rcBracer2;
+	case EqS_Belt:			rc += m_rcBelt;
+	case EqS_Ring1:		rc += m_rcRing1;
+	case EqS_Ring2:		rc += m_rcRing2;
+	case EqS_Ring3:		rc += m_rcRing3;
+	case EqS_Ring4:		rc += m_rcRing4;
+	case EqS_Boots:		rc += m_rcBoots;
+		default: return Rect(0,0,0,0);
+	}
+
+	return rc;
+}
+
+EquipmentSlot EquipmentDlg::GetSlot(int mx, int my, bool bScreenPos)
+{
+	if(m_pkEquipmentWnd == NULL)
+		return EqS_None;
+
+	Rect rc(0,0,0,0);
+	
+	if(bScreenPos)
+	{
+		mx -= m_pkEquipmentWnd->GetScreenRect().Left;
+		my -= m_pkEquipmentWnd->GetScreenRect().Top;
+	}
+	
+	if(m_rcHelm.Inside(mx,my)) return EqS_Helm;
+	if(m_rcGloves.Inside(mx,my)) return EqS_Gloves;
+	if(m_rcAmulett.Inside(mx,my)) return EqS_Amulett;
+	if(m_rcCloak.Inside(mx,my)) return EqS_Cloak;
+	if(m_rcWeaponHand.Inside(mx,my)) return EqS_WeaponHand;
+	if(m_rcBody.Inside(mx,my)) return EqS_Body;
+	if(m_rcShieldHand.Inside(mx,my)) return EqS_ShieldHand;
+	if(m_rcBracer1.Inside(mx,my)) return EqS_Bracer1;
+	if(m_rcBracer2.Inside(mx,my)) return EqS_Bracer2;
+	if(m_rcBelt.Inside(mx,my)) return EqS_Belt;
+	if(m_rcRing1.Inside(mx,my)) return EqS_Ring1;
+	if(m_rcRing2.Inside(mx,my)) return EqS_Ring2;
+	if(m_rcRing3.Inside(mx,my)) return EqS_Ring3;
+	if(m_rcRing4.Inside(mx,my)) return EqS_Ring4;
+	if(m_rcBoots.Inside(mx,my)) return EqS_Boots;
+
+	return EqS_None;	
+}
+
+void EquipmentDlg::Update(vector<MLContainerInfo>& vkItemList)
+{
+	char szItemName[128];
+	char text[64] = "";
+
+	// Remove all slots.
+	for(int i=0; i<m_vkEquipmentItemList.size(); i++)
+	{
+		ZGuiWnd* pkWnd = m_vkEquipmentItemList[i].pkWnd;
+		
+		if(pkWnd->GetSkin())
+			delete pkWnd->GetSkin();
+		
+		g_kMistClient.m_pkGui->GetToolTip()->RemoveToolTip(pkWnd);
+		g_kMistClient.m_pkGui->UnregisterWindow( pkWnd );
+	}
+
+	m_vkEquipmentItemList.clear();
+
+	Rect rc = GetRect(EqS_Helm, false);
+
+	sprintf(szItemName, "EquipmentItemLabel%i", 0);
+	ZGuiWnd* pkLabel = g_kMistClient.CreateWnd(Label, szItemName, text, 
+		m_pkEquipmentWnd, rc.Left, rc.Top, rc.Width(), rc.Height(), 0);
+	pkLabel->Show();
+
+	EQUIPMENT_SLOT kNewSlot;
+	kNewSlot.eSlotType = EqS_Helm;
+	kNewSlot.pkWnd = pkLabel;
+	m_vkEquipmentItemList.push_back(kNewSlot);
+
 }
--- zerofpsv2/src/mistclient2/gui_equipwnd.h
+++ zerofpsv2/src/mistclient2/gui_equipwnd.h
@@ -14,6 +14,27 @@ class Entity;
 #include <string>
 using namespace std;
 
+enum EquipmentSlot
+{
+	EqS_Helm				= 0,
+	EqS_Gloves			= 1,
+	EqS_Amulett			= 2,
+	EqS_Cloak			= 3,
+	EqS_WeaponHand		= 4,
+	EqS_Body				= 5,
+	EqS_ShieldHand		= 6,
+	EqS_Bracer1			= 7,
+	EqS_Bracer2			= 8,
+	EqS_Belt				= 9,
+	EqS_Ring1			= 10,
+	EqS_Ring2			= 11,
+	EqS_Ring3			= 12,
+	EqS_Ring4			= 13,
+	EqS_Boots			= 14,
+
+	EqS_None				= 99
+};
+
 class EquipmentDlg
 {
 public:
@@ -22,6 +43,8 @@ public:
 
 	void Open();
 	void Close();
+	void Update(vector<MLContainerInfo>& vkItemList);
+
 	void OnCommand(string strController);
 	void OnMouseMove(bool bLeftButtonPressed, int x, int y);
 	bool IsVisible() 
@@ -30,12 +53,52 @@ public:
 			return false; 
 		return m_pkEquipmentWnd->IsVisible(); 
 	}
+	void OnDrop(int mx, int my);
+	EquipmentSlot GetSlot(int mx, int my, bool bScreenPos);
+	Rect GetRect(EquipmentSlot eSlot, bool bScreenPos);
 		
 private:
+
+	struct EQUIPMENT_SLOT
+	{	
+		EquipmentSlot eSlotType;
+		ZGuiWnd* pkWnd;
+		int iItemID;
+		int iStackSize;
+	};
+
+	struct SPECIAL_SLOT
+	{
+		int m_iIndex; // index in item list
+	};
+
+	vector<EQUIPMENT_SLOT> m_vkEquipmentItemList;
+
+	SPECIAL_SLOT m_kMoveSlot; 
+
+	Point m_kCursorRangeDiff;
+	Point m_kItemWndPosBeforeMove; // fönster koordinater
 	
 	TextureManager* m_pkTexMan;
 	
 	ZGuiWnd* m_pkEquipmentWnd;	
+
+	// Kollisionsrektanglar för olika itemslots i fönsterkoordinater.
+	const Rect m_rcHelm;
+	const Rect m_rcGloves;
+	const Rect m_rcAmulett;
+	const Rect m_rcCloak;
+	const Rect m_rcWeaponHand;
+	const Rect m_rcBody;
+	const Rect m_rcShieldHand;
+	const Rect m_rcBracer1;
+	const Rect m_rcBracer2;
+	const Rect m_rcBelt;
+	const Rect m_rcRing1;
+	const Rect m_rcRing2;
+	const Rect m_rcRing3;
+	const Rect m_rcRing4;
+	const Rect m_rcBoots;
 };
 
 #endif // _GUI_EQUIPMENTDLG_H_
