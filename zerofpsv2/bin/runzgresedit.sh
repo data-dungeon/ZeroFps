fc6ec536b7520639108a3b4f9211f11cef7bb8de
dvoid <dvoid>
2005-03-02 16:28:32 +0000

    *** empty log message ***

--- zerofpsv2/bin/runzgresedit.sh
+++ zerofpsv2/bin/runzgresedit.sh
@@ -5,7 +5,7 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
 
 cd `dirname $0`
-./zgresedit.bin $2 $3 $4 $5 $6 $7 $8 $9
+./zguiresed.bin $2 $3 $4 $5 $6 $7 $8 $9
 
 export LD_LIBRARY_PATH=$templdpath
 
