7faa82c1ef5458ea99ff71253dca33986f6be6c2
zeblar <zeblar>
2005-04-30 17:13:40 +0000

    *** empty log message ***

--- zerofpsv2/src/mistclient2/skillbar.cpp
+++ zerofpsv2/src/mistclient2/skillbar.cpp
@@ -6,42 +6,23 @@ extern MistClient	g_kMistClient;
 
 SkillBar::SkillBar()
 {
-	cout<<"skillbar created"<<endl;
-
-
-/*
-	ZGuiLabel* m_pkIconSelection = (ZGuiLabel*)g_kMistClient.CreateWnd(Label,"test", "SkillBar", "", 0, 0, 32, 32, 0);
-
-// 	m_pkIconSelection->SetSkin(new ZGuiSkin());
-// 	m_pkIconSelection->GetSkin()->m_iBkTexID = g_kMistClient.LoadGuiTextureByRes(
-// 		"actions/action_selection_border.tga") ;	
-
-	m_pkIconSelection->SetText("hej hopp",true);
-	m_pkIconSelection->Show();*/
-
 }
 
 void SkillBar::Update()
 {
-// 	if(ZGuiWnd* pkWnd= (ZGuiWnd*)g_kMistClient.GetWnd("SkillBar"))
-// 	{
-// 		pkWnd->SetPos(0,50);	
-// // 		pkWnd->	
-// 	}
-// 	else
-// 	{
-// 		cout<<"ERROR------------------ FUCK"<<endl;
-// 	}
-// 	
-// 	if(!g_kMistClient.GetWnd("testtest"))
-// 	{
-// 		ZGuiLabel* m_pkIconSelection = (ZGuiLabel*)g_kMistClient.CreateWnd(Label,"testtest", "SkillBar", "", 0, 0, 32, 32, 0);
-// 	
-// 	// 	m_pkIconSelection->SetSkin(new ZGuiSkin());
-// 	// 	m_pkIconSelection->GetSkin()->m_iBkTexID = g_kMistClient.LoadGuiTextureByRes(
-// 	// 		"actions/action_selection_border.tga") ;	
-// 	
-// 		m_pkIconSelection->SetText("hej hopp",true);
-// 		m_pkIconSelection->Show();
-// 	}
+	ZGuiWnd* pkSkillbar = (ZGuiWnd*)g_kMistClient.GetWnd("SkillBar");
+
+	static bool s_bOnes = false;
+
+ 	if(pkSkillbar && s_bOnes == false)
+ 	{
+		s_bOnes = true;
+
+ 		ZGuiLabel* m_pkIconSelection = (ZGuiLabel*)g_kMistClient.CreateWnd(Label,
+			"testtest", "SkillBar", "SkillBar", 0, 0, 32, 32, 0);
+ 	
+ 	 	m_pkIconSelection->SetSkin(new ZGuiSkin());
+ 	 	m_pkIconSelection->GetSkin()->m_iBkTexID = g_kMistClient.LoadGuiTextureByRes(
+ 	 		"actions/action_selection_border.tga") ;
+ 	}
 }
