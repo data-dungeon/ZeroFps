ec68d111405a3eb5fad933d397ace975722e7754
dvoid <dvoid>
2005-05-09 19:30:52 +0000

    *** empty log message ***

--- zerofpsv2/bin/runmadmake.sh
+++ zerofpsv2/bin/runmadmake.sh
@@ -2,10 +2,10 @@
 
 echo Running in workdirectory, and setting LD_LIBRARY_PATH to curent direcotry
 export templdpath=$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
-
-cd `dirname $0`
-./madmake.bin $@ 
+#export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`dirname $0`:$LD_LIBRARY_PATH
+#cd `dirname $0`
+`dirname $0`/madmake.bin $@ 
 
 export LD_LIBRARY_PATH=$templdpath
 
