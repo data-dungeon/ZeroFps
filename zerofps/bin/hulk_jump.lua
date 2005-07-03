6026b9a26bebca581e8ab0f94999dd704a27b943
cvs-fast-export <cvs-fast-export>
2005-07-03 19:41:50 +0000

    Synthetic commit for incomplete tag ms4

--- zerofps/bin/hulk_jump.lua
+++
@@ -1,5 +0,0 @@
--- script test for game
-function HulkJump()
-	player_pos_y = player_pos_y + 40
-	Print("apa");
-end
--- zerofps/src/tool/madmake/main.cpp
+++
@@ -1,8 +0,0 @@
-int HATASDL(int argc, char* argv[]);
-
-int main(int argc, char* argv[])
-{
-	return HATASDL(argc, argv);
-}
-
-
